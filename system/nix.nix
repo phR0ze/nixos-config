4d45f051fd2f6e06c79dd3d9809f565726629440
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-02-15 14:02:14 -0700

    Disable git dirty warning

--- system/nix.nix
+++ system/nix.nix
@@ -2,9 +2,29 @@
 #---------------------------------------------------------------------------------------------------
 { pkgs, ... }:
 {
+#  nixpkgs = {
+#    overlays =
+#      builtins.attrValues outputs.overlays
+#      ++ [
+#        inputs.nixneovimplugins.overlays.default
+#        inputs.nur.overlay
+#        inputs.neovim-nightly-overlay.overlay
+#        inputs.nixgl.overlay
+#        inputs.codeium.overlays."x86_64-linux".default
+#      ];
+#
+#    config = {
+#      allowUnfree = true;
+#      allowUnfreePredicate = _: true;
+#    };
+#  };
+
   nix = {
     package = pkgs.nixFlakes;
-    settings.experimental-features = [ "nix-command" "flakes" ]; # enable flake support
+    settings = {
+      warn-dirty = false;
+      experimental-features = [ "nix-command" "flakes" ]; # enable flake support
+    };
     extraOptions = "experimental-features = nix-command flakes";
   };
 }
