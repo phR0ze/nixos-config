8b04f42c0e0312750311e1c53e860e48019d420c
phR0ze <prc451@gmail.com>
2024-02-23 17:17:57 -0700

    Rename bootable

--- profiles/base/minimal.nix
+++ profiles/base/minimal.nix
@@ -15,12 +15,12 @@
   xdg.mime.enable = mkDefault false;
   xdg.sounds.enable = mkDefault false;
 
-  # Keeping docs enabled
-  #documentation.enable = mkDefault false;
-  #documentation.doc.enable = mkDefault false;
-  #documentation.info.enable = mkDefault false;
-  #documentation.man.enable = mkDefault false;
-  #documentation.nixos.enable = mkDefault false;
+  # Disable docs to save space
+  documentation.enable = mkDefault false;
+  documentation.doc.enable = mkDefault false;
+  documentation.info.enable = mkDefault false;
+  documentation.man.enable = mkDefault false;
+  documentation.nixos.enable = mkDefault false;
 
   # Disable default packges like Perl by default
   environment.defaultPackages = mkDefault [ ];
@@ -30,8 +30,8 @@
   programs.less.lessopen = mkDefault null;  # less pulls in Perl
   boot.enableContainers = mkDefault false;  # nixos-containers pulls in Perl
   programs.command-not-found.enable = mkDefault false;
-  #services.logrotate.enable = mkDefault false; # keeping essential tooling
-  #services.udisks2.enable = mkDefault false; # keeping essential tooling
+  services.logrotate.enable = mkDefault false;
+  services.udisks2.enable = mkDefault false;
 
   # Trim down the boot modules
   boot.initrd.includeDefaultModules = false;
