a19a25f28ce90ec9fb9993f58797ac9e9dc55c48
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-02-26 07:44:56 -0700

    Adding git and vim to install

--- profiles/base/bootable.nix
+++ profiles/base/bootable.nix
@@ -41,6 +41,51 @@
     # Set the NixOS version that this was installed with
     system.stateVersion = args.systemSettings.stateVersion;
   };
+
+
+  # Base system packages
+  environment.systemPackages = [
+#    pkgs.w3m-nographics # needed for the manual anyway
+#    pkgs.testdisk # useful for repairing boot problems
+#    pkgs.ms-sys # for writing Microsoft boot sectors / MBRs
+#    pkgs.efibootmgr
+#    pkgs.efivar
+#    pkgs.parted
+#    pkgs.gptfdisk
+#    pkgs.ddrescue
+#    pkgs.ccrypt
+#    pkgs.cryptsetup # needed for dm-crypt volumes
+    pkgs.git                                      # Required for Flakes support
+
+    # Some text editors.
+    (pkgs.vim.customize {
+      name = "vim";
+      vimrcConfig.packages.default = {
+        start = [ pkgs.vimPlugins.vim-nix ];
+      };
+      vimrcConfig.customRC = "syntax on";
+    })
+
+#    # Some networking tools.
+#    pkgs.fuse
+#    pkgs.fuse3
+#    pkgs.sshfs-fuse
+#    pkgs.socat
+#    pkgs.screen
+#    pkgs.tcpdump
+#
+#    # Hardware-related tools.
+#    pkgs.sdparm
+#    pkgs.hdparm
+#    pkgs.smartmontools # for diagnosing hard disks
+#    pkgs.pciutils
+#    pkgs.usbutils
+#    pkgs.nvme-cli
+#
+#    # Some compression/archiver tools.
+#    pkgs.unzip
+#    pkgs.zip
+  ];
 }
 
 # vim:set ts=2:sw=2:sts=2
