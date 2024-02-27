6a362820fdcc849c3839a902e2eafd6382d36894
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-02-27 09:18:00 -0700

    Working on dircolors

---
+++ modules/terminal/dircolors.nix
@@ -0,0 +1,13 @@
+# Dircolors configuration
+#
+# ### Features
+#---------------------------------------------------------------------------------------------------
+{ ... }:
+{
+  programs.dircolors = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+}
+
+# vim:set ts=2:sw=2:sts=2
