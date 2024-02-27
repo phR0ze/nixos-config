124143692252309d9f3e6383d0237c8536896f46
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-02-27 16:06:32 -0700

    Working on HM

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -5,25 +5,35 @@
 # header of any home-manager modules i.e. nix files that are required to be called from home-manager
 # to handle home-manager specific options.
 #---------------------------------------------------------------------------------------------------
-{ args, ... }:
+{ pkgs, args, ... }:
 {
   imports = [
     args.home-manager.nixosModules.home-manager
   ];
 
   home-manager = {
+    # By default Home Manager uses its own private pkgs instance. By setting this value to true
+    # Home Manager will instead use the global pkgs configured by your system flake.
     useGlobalPkgs = true;
-    useUserPackages = false;
-    extraSpecialArgs = { inherit args; }; # passes 'args' to home-manager modules
 
-    # Define root user
+    # Don't use the users.users.<name>.packages option
+    useUserPackages = false;
+
+    # This passes 'args' to home-manager modules in a similar way that specialArgs at the top
+    # level passes args into this nix module.
+    extraSpecialArgs = { inherit args; };
+
+    # Defines modules to be used for all users except for root
+    sharedModules = [
+      ../terminal/dircolors.nix
+    ];
 
     # Define system user
-    users.${args.settings.username} = { args, ... }:
+    users.${args.settings.username} = { pkgs, args, ... }:
     {
-      imports = [
-        ../terminal/dircolors.nix
-      ];
+      #imports = [
+      #  ../terminal/dircolors.nix
+      #];
     
       home = {
         username = "${args.settings.username}";
