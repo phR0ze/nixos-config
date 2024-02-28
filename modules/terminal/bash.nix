de942615a78456bfb2d99c52c7bf48bf2ae719c9
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-02-28 14:16:20 -0700

    Adding starship config

--- modules/terminal/bash.nix
+++ modules/terminal/bash.nix
@@ -30,36 +30,30 @@ in
     # Adds these to the bottom of /etc/bashrc
     shellAliases = aliases;
 
-    # Enable starship
-    # promptInit = ''
-    #   eval "$(${pkgs.starship}/bin/starship init bash)"
-    # '';
-
-    # Adds in the middle of /etc/bashrc
+    # Use green prompt for admin and red for user
+    # This will get overriden by the starship configuration anyway
     promptInit = ''
-      if [ "$TERM" != "dumb" ]; then
-        if [ "$TERM" == "xterm"]; then
-          PS1="\[\033]2;\h:\u:\w\007\]$PS1"
-        else
-          PROMPT_COLOR="1;31m"
-          ((UID)) && PROMPT_COLOR="1;32m"
-          PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
-        fi
-      fi
+      PROMPT_COLOR="1;31m"
+      ((UID)) && PROMPT_COLOR="1;32m"
+      PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
     '';
 
+    # Adds this to /etc/profile for all shells
     shellInit = ''
-      complete -cf sudo                   # Setup bash tab completion for sudo
-      set -o vi                           # Set vi command prompt editing
       umask 022                           # Default permissions for the files you create
-
       shopt -s dotglob                    # Have * include .files as well
       shopt -s extglob                    # Include extended globbing support
+    '';
+
+    # Adds this to the /etc/bashrc
+    interactiveShellInit = ''
+      complete -cf sudo                   # Setup bash tab completion for sudo
+      set -o vi                           # Set vi command prompt editing
       shopt -s histappend                 # Append to the history file, don't overwrite it
       shopt -s checkwinsize               # Update window LINES/COLUMNS after ea. command if necessary
 
       export HISTSIZE=10000               # Set history length
-      export HISTFILESIZE=${HISTSIZE}     # Set history file size
+      export HISTFILESIZE=$HISTSIZE       # Set history file size
       export HISTCONTROL=ignoreboth       # Ignore duplicates and lines starting with space
       export EDITOR=vim                   # Set the editor to use
       export VISUAL=vim                   # Set the editor to use
@@ -68,9 +62,9 @@ in
       export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
     '';
 
+    # Adds this to the /etc/profile as well
     #loginShellInit = ''
-
-    #interactiveShellInit = ''
+    #'';
   };
 }
 
