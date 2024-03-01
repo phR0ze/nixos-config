6a46fbeae1aaaa02c66980694e6fa224f8832d46
phR0ze <prc451@gmail.com>
2024-03-01 14:30:22 -0700

    Working on a non home-manager based installer

--- profiles/iso/default.nix
+++ profiles/iso/default.nix
@@ -8,50 +8,34 @@
 { args, pkgs, lib, ... }:
 {
   imports = [
-    # Import and activate home-manager, this needs to be done before home manager options will be
-    # detected and handled by home-manager.
-    args.home-manager.nixosModules.home-manager
-
     # I get a weird infinite recursion bug if I use ${pkgs} instead
     "${args.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
     ../../modules/nix.nix
   ];
 
-  # Configure home-manager for ISO to have a script that is automatically run on login
-  # to launcher our installer automation 'clu'
-  home-manager = {
-    extraSpecialArgs = { inherit args; };
-    users.nixos = {
-      home.file.".bash_profile".text = ''
-        # Wait for the network to be live
-        echo ":: Checking if network is ready"
-        while ! ping -c 4 google.com &>/dev/null; do
-          echo "   Waiting on the network"
-          sleep 1
-        done
-        echo "   Network is ready"
+  # Configure /etc/bashrc to launchr our installer automation 'clu'
+  programs.bash.promptPluginInit = ''
+    # Wait for the network to be live
+    echo ":: Checking if network is ready"
+    while ! ping -c 4 google.com &>/dev/null; do
+      echo "   Waiting on the network"
+      sleep 1
+    done
+    echo "   Network is ready"
 
-        # Clone the installer repo as needed
-        echo ":: Checking for the nixos-config repo"
-        if [ ! -d /home/nixos/nixos-config ]; then
-          echo "   Downloading https://github.com/phR0ze/nixos-config"
-          git clone https://github.com/phR0ze/nixos-config /home/nixos/nixos-config
-        fi
-        [ -f /home/nixos/nixos-config/clu ] && echo "   Installer script exists"
+    # Clone the installer repo as needed
+    echo ":: Checking for the nixos-config repo"
+    if [ ! -d /home/nixos/nixos-config ]; then
+      echo "   Downloading https://github.com/phR0ze/nixos-config"
+      git clone https://github.com/phR0ze/nixos-config /home/nixos/nixos-config
+    fi
+    [ -f /home/nixos/nixos-config/clu ] && echo "   Installer script exists"
 
-        # Execute the installer script
-        echo ":: Executing the installer script"
-        chmod +x /home/nixos/nixos-config/clu
-        sudo /home/nixos/nixos-config/clu install
-      '';
-
-      home = {
-        username = "nixos";
-        homeDirectory = "/home/nixos";
-        stateVersion = args.settings.stateVersion;
-      };
-    };
-  };
+    # Execute the installer script
+    echo ":: Executing the installer script"
+    chmod +x /home/nixos/nixos-config/clu
+    sudo /home/nixos/nixos-config/clu install
+  '';
 
   # Set the default user passwords
   users.users.nixos.password = "nixos";
