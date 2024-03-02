73f6933c2d3ab1b060ae73bcfe78c0019d23f98b
phR0ze <prc451@gmail.com>
2024-03-01 18:13:21 -0700

    Adding alsa-firmware to cli default profile

--- profiles/x11/default.nix
+++ profiles/x11/default.nix
@@ -20,7 +20,14 @@
     #];
 
     # Arch Linux recommends libinput be enabled
-    libinput.enable = true;
+    libinput = {
+      enable = true;
+#      touchpad = {
+#        tappingDragLock = false;
+#        naturalScrolling = true;
+#      };
+    };
+
 
     # Video drivers to be tried in order until one that supports your card is found
     # Default: modesetting, fbdev
@@ -36,9 +43,53 @@
     displayManager.startx.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [
+  # Configure sound
+  sound.enable = true;
+  hardware.pulseaudio = {
+		enable = true;
+		package = pkgs.pulseaudioFull;
+    daemon.config = { flat-volumes = "no"; };
+	};
 
-   # git                           # The fast distributed version control system
+  # ??
+  security.polkit = {
+		enable = true;
+		extraConfig = ''
+			polkit.addRule(function(action, subject) {
+				if (subject.isInGroup("wheel")) {
+					return polkit.Result.YES;
+				}
+			});
+		'';
+	};
+
+  fonts = {
+    fontDir.enable = true;
+    fonts = with pkgs;[
+      corefonts
+      inconsolata
+      terminus_font
+      dejavu_fonts
+      ubuntu_font_family
+      source-code-pro
+      source-sans-pro
+      source-serif-pro
+      roboto-mono
+      roboto
+      overpass
+      libre-baskerville
+      font-awesome
+      julia-mono
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    pavucontrol                   # PulseAudio Volume Control
+    vaapiIntel
+    vaapi-intel-hybrid
+    libva-full
+    libva-utils
+    intel-media-driver
   ];
 }
 
