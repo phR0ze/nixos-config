16614a634d79a71703caa169f4a3a99d28b8f2f2
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-03-04 10:43:21 -0700

    Starting from clean xfce

--- profiles/xfce/default.nix
+++ profiles/xfce/default.nix
@@ -49,11 +49,11 @@
 #  ];
   environment.systemPackages = with pkgs; [
     jdk17
-    (prismlauncher.override (prev: {
-      prismlauncher-unwrapped = prev.prismlauncher-unwrapped.overrideAttrs (o: {
-        patches = (o.patches or [ ]) ++ [ ../../patches/prismlauncher/offline.patch ];
-      });
-    }))
+#    (prismlauncher.override (prev: {
+#      prismlauncher-unwrapped = prev.prismlauncher-unwrapped.overrideAttrs (o: {
+#        patches = (o.patches or [ ]) ++ [ ../../patches/prismlauncher/offline.patch ];
+#      });
+#    }))
   ];
 
     #config = lib.mkAfter ''
