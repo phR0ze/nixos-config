87faa7345f8349c7a09bec6bc164bc449ea626d4
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-03-05 21:04:59 -0700

    Working on console font

--- profiles/xfce/default.nix
+++ profiles/xfce/default.nix
@@ -135,26 +135,6 @@
 #		'';
 #	};
 #
-#  fonts = {
-#    fontDir.enable = true;
-#    fonts = with pkgs;[
-#      corefonts
-#      inconsolata
-#      terminus_font
-#      dejavu_fonts
-#      ubuntu_font_family
-#      source-code-pro
-#      source-sans-pro
-#      source-serif-pro
-#      roboto-mono
-#      roboto
-#      overpass
-#      libre-baskerville
-#      font-awesome
-#      julia-mono
-#    ];
-#  };
-
 }
 
 # vim:set ts=2:sw=2:sts=2
