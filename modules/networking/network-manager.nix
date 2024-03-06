c5ec9346a7277b98d83f096d1abf2d365845c5a8
phR0ze <prc451@gmail.com>
2024-03-05 19:40:19 -0700

    Move network manager into module

--- modules/networking/network-manager.nix
+++ modules/networking/network-manager.nix
@@ -5,8 +5,13 @@
 { args, ... }:
 {
   networking = {
-    networkmanager.enable = true;             # easiest way to get networking up and runnning
+    networkmanager.enable = true;
+  };
+
+  # Enables ability for user to make network manager changes
+  users.users.${args.settings.username} = {
+    extraGroups = [ "networkmanager" ];
   };
 }
-
+ 
 # vim:set ts=2:sw=2:sts=2
