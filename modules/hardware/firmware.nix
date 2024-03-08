a125ffa27e8798c91691baaaea9d243a0e71f414
phR0ze <prc451@gmail.com>
2024-03-08 10:22:01 -0700

    Adding extra firmware

---
+++ modules/hardware/firmware.nix
@@ -0,0 +1,20 @@
+# Additional firmware
+#
+# ### Detail
+#---------------------------------------------------------------------------------------------------
+{ pkgs, ... }:
+{
+  # - 'hardware.enableRedistributableFirmware = true;' is just a short cut for the below list
+  hardware.firmware = with pkgs; [
+    linux-firmware
+    intel2200BGFirmware
+    rtl8192su-firmware
+    rt5677-firmware
+    rtl8761b-firmware
+    rtw88-firmware
+    zd1211fw
+    alsa-firmware
+    sof-firmware
+    libreelec-dvb-firmware
+  ];
+}
