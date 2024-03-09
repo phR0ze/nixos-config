242b7f37feaeca6fc429c2968a4b0fb79aef6346
phR0ze <prc451@gmail.com>
2024-03-09 11:10:49 -0700

    Working on xfce configuration

--- modules/desktop/xfce.nix
+++ modules/desktop/xfce.nix
@@ -1,7 +1,26 @@
 # XFCE configuration
 #
 # ### Details
+# - https://xfce.readthedocs.io/en/latest/core/xfconf/index.html
+# - https://www.spurint.org/journal/2008/10/xfconf-a-new-configuration-storage-system
 #---------------------------------------------------------------------------------------------------
 { ... }:
 {
+  programs.xfconf.enable = true;
+
+#  xfconf.settings = {
+#    xsettings = {
+#      "Gdk/WindowScalingFactor" = 2;
+#      "Xft/DPI" = 100;
+#      "Xfce/LastCustomDPI" = 100;
+#    };
+#    xfce4-session = {
+#      "startup/ssh-agent/enabled" = false;
+#      "general/LockCommand" = "${pkgs.lightdm}/bin/dm-tool lock";
+#    };
+#    xfce4-desktop = {
+#      "backdrop/screen0/monitorLVDS-1/workspace0/last-image" =
+#        "${pkgs.nixos-artwork.wallpapers.stripes-logo.gnomeFilePath}";
+#    };
+#  };
 }
