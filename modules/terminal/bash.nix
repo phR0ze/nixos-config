91bb8b9b5d6484d5a9cbbe929dd15804d14ac463
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-03-12 15:14:34 -0600

    Test copying files

--- modules/terminal/bash.nix
+++ modules/terminal/bash.nix
@@ -5,6 +5,12 @@
 #---------------------------------------------------------------------------------------------------
 { config, lib, pkgs, ... }:
 {
+  environment.variables = {
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_DATA_HOME = "$HOME/.local/share";
+  };
+
   programs.bash = {
 
     # Configures 'dircolors -b' but doesn't allow for a custom config so manually doing this below.
