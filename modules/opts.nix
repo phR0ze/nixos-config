cda85188403f5867f21cb2dffa8148d8e96691ef
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-03-12 14:50:51 -0600

    Testing userActivationScripts

--- modules/opts.nix
+++ modules/opts.nix
@@ -3,7 +3,7 @@
 # ### Features
 #---------------------------------------------------------------------------------------------------
 { options, config, lib, pkgs, args, ... }: with lib;
-
+{
   # We can leverage the write builders to create files in the user's home directory
   # https://nixos.org/manual/nixpkgs/stable/#trivial-builder-writeText
 
@@ -35,10 +35,38 @@
 #    });
 #  };
 #}
-let
-  foo = pkgs.writeShellScriptBin "foobar" ''
-    echo "{env}`HOME`"
-  '';
-in {
-  environment.systemPackages = [ foo ];
+  let
+
+    # Files activation script
+    activationScript = ''
+      # Ensure xdg environment vars are set
+      XDG_CONFIG_HOME=''${XDG_CONFIG_HOME:-$HOME/.config}
+      XDG_CACHE_HOME=''${XDG_CACHE_HOME:-$HOME/.cache}
+      XDG_DATA_HOME=''${XDG_DATA_HOME:-$HOME/.local/share}
+      XDG_STATE_HOME=''${XDG_STATE_HOME:-$HOME/.local/state}
+
+      echo "this is a test" > ''${XDG_CONFIG_HOME}/foobar
+
+      # xdg-desktop-settings generates this empty file but
+      #rm -fv ''${XDG_CONFIG_HOME}/menus/applications-merged/xdg-desktop-menu-dummy.menu
+
+      #trolltech_conf="''${XDG_CONFIG_HOME}/Trolltech.conf"
+      #if [ -e "$trolltech_conf" ]; then
+      #  ${getBin pkgs.gnused}/bin/sed -i "$trolltech_conf" -e '/nix\\store\|nix\/store/ d'
+      #fi
+    '';
+  in
+  {
+    # User activation scripts
+    # ----------------------------------------------------------------------------------------------
+    # system.userActivationScripts are executed by a systemd user service when a nixos-rebuild switch 
+    # is run and likewise every boot since each boot activates the system anew.
+    # 
+    # - https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/system/activation/activation-script.nix#L145
+    config.system.userActivationScripts.files = activationScript;
+#    config = {
+#      system.activationScripts.files =
+#        stringAfter [ "etc" "users" "groups" ] config.system.build.etcActivationCommands;
+#    };
+  }
 }
