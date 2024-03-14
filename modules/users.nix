a69a936ef8557c5998dc1d8c40268f1995f83ea1
phR0ze <prc451@gmail.com>
2024-03-14 17:56:02 -0600

    Trying alternate conditional syntax

--- modules/users.nix
+++ modules/users.nix
@@ -20,11 +20,7 @@
 
   # Initialize user home from /etc/skel on first login
   # ------------------------------------------------------------------------------------------------
-  files.all."root/foo" = {
-    enable = false;
-    kind = "dir";
-    text = "foo";
-  };
+  files.all."root/foo".kind = "dir";
 
   files.all."root/foobar1" = {
     enable = true;
