0d22bb4ca3cd34221c8f04b37881bb5e615cd96f
phR0ze <prc451@gmail.com>
2024-03-16 22:00:22 -0600

    Working on new root option

--- options/files/root.nix
+++ options/files/root.nix
@@ -1,149 +1,28 @@
-# All files option
-# Install files to your system either as writable files or links to readonly nix store paths.
+# Root option
+# Wraps the all option to make all the paths relative to the /root path to simplify root user 
+# configuration options.
 #
-# ### Disclaimer
-# This is NixOS specific and doesn't support other distributions or crossplatform systems. My intent 
-# here is to provide some basic file manipulation for deploying configuration already stored in a git 
-# repo. I specifically chose not to use Home Manager to keep this simple and avoid the complexity of 
-# that solution.
-
-# ### Warnings
-# - gets run on boot and on nixos-rebuild switch so be careful what is included here
-# - file type overwrites any existing target to keep the configuration accurage
-# - removes owned files and directories when they are no longer in your configuration
-# - multiple configurations for the same file or directory will be handled in a last out wins which
-#   can be non-deterministic. best to not duplicate files or directory configurations for now.
-#
-# ### Features
-# - pseudo atomic link switching via the /nix/files indirection link
-# - merges with existing directories overwriting files that conflict but leaving unowned files
-# - choose to install files as writable files or as links to readonly nix store paths
-# - choose to own the files or directories, defaults to owning files and not owning directories
-# - choose the mode, user, and group for files and or directories
-# - only recreates links when they are incorrect or missing
-# - supports spaces in file or directory names
-#
-# ### Implementation Details
-# This option follows a similar pattern as the environment.etc option. The pattern consists of three 
-# different components.
-#  1. The option is defined here in this file
-#  2. The option is imported into your project at the top level making it available everywhere
-#  3. The option is invoked e.g. files.all."/root/foobar1".text = "this is a foobar1 test";
-#  4. During nixos-rebuild switch the activationScripts get run performing the install
-#  5. This in turn invokes the filesPackage via the reference
-#  6. The files aggregate attribute set adds them to the /nix/store and links them in the parent package
-#  7. The parent filesPackage package is then added to the /nix/store
-#  8. Finally the original config.system.activationScripts.files payload is executed with the 
-#     filesPackage as a parameter
-#  9. The activation install script then uses the filesPackage to install the files
+# see ./all.nix for warnings and instructions
 #---------------------------------------------------------------------------------------------------
 { options, config, lib, pkgs, args, ... }: with lib;
-let
-
-  # Filter the files calls down to just those that are enabled
-  files' = filter (f: f.enable) (attrValues config.files.all);
-
-  # Using runCommand to build a derivation that bundles the target files into a /nix/store package 
-  # that we can then use during the activation later on to deploy the files to their indicated 
-  # destination paths.
-  # - $HOME is not a valid value
-  # - operation is run as the root user
-  # - files and directories are owned by root by default
-  # ------------------------------------------------------------------------------------------------
-  filesPackage = pkgs.runCommandLocal "files" {} ''
-    set -euo pipefail             # Configure an immediate fail if something goes badly
-    mkdir -p "$out"               # Creates the nix store path to populate
-
-    track() {
-      local src="$1"              # Source e.g. '/nix/store/23k9zbg0brggn9w40ybk05xw5r9hwyng-files-root-foobar'
-      local dst="$2"              # Destination path to deploy to e.g. 'root/foobar'
-      local kind="$3"             # Kind of file being created [ copy | file | dir ]
-      local dirmode="$4"          # Mode to use for directories
-      local filemode="$5"         # Mode to use for files
-      local user="$6"             # Owner to use for file and/or directories
-      local group="$7"            # Group to use for file and/or directories
-      local own="$8"              # Own the the file or directory
-      local op="$9"               # Operation type [ contents | direct ]
-
-      # Validation on inputs
-      [[ ''${dst:0:1} == "/" ]] && echo "paths must not start with a /" && exit 1
-      [[ ''${dst:0-1} == "/" ]] && echo "paths must not end with a /" && exit 1
-      [[ "$dst" == *".meta" ]] && echo "paths must not end with .meta" && exit 1
-      [[ ! -d "$src" && "$kind" == "dir" ]] && echo "store path $src is a file and kind is dir" && exit 1
-
-      echo "Linking: $src -> $out/$dst"
-
-      # Determine meta name
-      local meta
-      if [[ -d "$src" ]]; then
-        meta="$out/$dst.meta.dir"                       # craft meta file for directory
-      else
-        meta="$out/$dst.meta.file"                      # craft meta file name for files
-      fi
-
-      # Link nix store path
-      local dir="$(dirname "$dst")"                     # grab the directory of the target
-      [[ ''${dir} != "." ]] && mkdir -p "$out/$dir"     # create any needed directories
-      ln -sf "$src" "$out/$dst"                         # link in the file content
-
-      # Add the metadata file content
-      echo "Metadata: $meta"
-      echo "$op" >> "$meta"
-      echo "$kind" >> "$meta"
-      echo "$src" >> "$meta"
-      echo "$dirmode" >> "$meta"
-      echo "$filemode" >> "$meta"
-      echo "$user" >> "$meta"
-      echo "$group" >> "$meta"
-      [[ $own -eq 1 ]] && echo "true" >> "$meta" || echo "false" >> "$meta"
-    }
-
-    # Convert the files derivations into a list of calls to track by taking all the file
-    # derivations escaping the arguments and adding them line by line to this ouput bash script.
-    # e.g. 'track' '/nix/store/<hash>-files-root-foobar' '/root/foobar'
-    ${concatMapStringsSep "\n" (entry: escapeShellArgs [
-      "track"
-      # Simply referencing the source file here will suck it into the /nix/store as its own package
-      "${entry.source}"
-      entry.target
-      entry.kind
-      entry.dirmode
-      entry.filemode
-      entry.user
-      entry.group
-      entry.own
-      entry.op
-    ]) files'}
-  '';
-
-  # Add the installer script to the /nix/store for reference
-  # ------------------------------------------------------------------------------------------------
-  installScript = pkgs.writeShellScript "installScript"
-    (lib.fileContents ./install);
-in
 {
   options = {
-
-    # all files option
-    # ----------------------------------------------------------------------------------------------
-    files.all = mkOption {
+    files.root = mkOption {
       description = lib.mdDoc ''
-        Set of files to deploy in the target system.
-        - destination paths must be relative to the root e.g. etc/foo
-        - only one operation can be used at at time [ copy | link | dir | text | copyIn | linkIn ]
+        Set of files to deploy to the root user's directory
+        - destination paths must be relative to /root e.g. .config
       '';
       example = ''
         # Create a single file from raw text
-        files.all."root/.dircolors".text = "this is a test";
+        files.root.".dircolors".text = "this is a test";
 
         # Include a local file as your target
-        files.all."root/.dircolors".copy = ../include/home/.dircolors;
+        files.root.".dircolors".copy = ../include/home/.dircolors;
 
         # Multi file example
-        files.all = {
-          "etc/asound.conf".text = "autospawn=no";
+        files.root = {
+          "root/.config".copyIn = ../include/home/.config;
           "root/.dircolors".copy = ../include/home/.dircolors;
-          "etc/openal/alsoft.conf".link =  ../include/etc/openal/alsoft.conf;
         };
       '';
 
