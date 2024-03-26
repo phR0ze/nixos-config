2f8ecf494ffddd6023ab2894318a3a7d5c8a6af4
phR0ze <prc451@gmail.com>
2024-03-26 16:57:40 -0600

    Test removing configs

--- options/files/user.nix
+++ options/files/user.nix
@@ -39,7 +39,7 @@ in
 
   # Merge the files.user options into files.any options
   # ----------------------------------------------------------------------------------------------
-  config.files.any = mkIf (config.files ? "user") (mkMerge [
+  config.files.any = mkMerge [
     # We are changing the user sets names to the unique target definitions set above to avoid 
     # clashing with values set in other options. Potentially we could be setting 
     # 'files.user.".dircolors"' and 'files.root.".dircolors"' or others. This gives them a unique 
@@ -49,8 +49,8 @@ in
     #   error: The option `files.any.".dircolors".target' has conflicting definition values:
     #   - In `/nix/store/vcgagc2la95ngp00296x0ac69s9d0vmx-source/options/files/root.nix': "root/.dircolors"
     #   - In `/nix/store/vcgagc2la95ngp00296x0ac69s9d0vmx-source/options/files/user.nix': "home/admin/.dircolors"
-    #(if (config.files ? "user") then  { foo = "bar"; } else {})
-    #(if (config.files ? "user") then attrsets.mapAttrs' (name: value: nameValuePair (value.target) value) 
-    # config.files.user else {})
-  ]);
+    (mkIf (config.files ? "user")
+      (attrsets.mapAttrs' (name: value: nameValuePair (value.target) value) config.files.user)
+    )
+  ];
 }
