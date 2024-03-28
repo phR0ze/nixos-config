217a87c3a883571ae767ca741213deac4b4cfc83
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-03-28 17:50:37 -0600

    Working on offline installer

--- profiles/iso/default.nix
+++ profiles/iso/default.nix
@@ -19,9 +19,45 @@
   # Original example naming: "nixos-23.11.20240225.5bf1cad-x86_64-linux.iso"
   isoImage.isoBaseName = "nixos-installer";
   isoImage.isoName = "${config.isoImage.isoBaseName}-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}.iso";
-  isoImage.includeSystemBuildDependencies = true;
-  isoImage.storeContents = [
+  #isoImage.includeSystemBuildDependencies = true; # includes source code; i just want packages
+  isoImage.storeContents = with pkgs; [
     config.system.build.toplevel   # default ISO inclusion
+    
+    # Networking utilities
+    git                           # The fast distributed version control system
+    nfs-utils                     # Support programs for Network File Systems
+    wget                          # Retrieve files using HTTP, HTTPS, and FTP
+
+    # System utilities
+    efibootmgr                    # EFI Boot Manager
+    efivar                        # Tools to manipulate EFI variables
+    cdrtools                      # ISO tools e.g. isoinfo, mkisofs
+    ddrescue                      # GNU ddrescue, a data recovery tool
+    dos2unix                      # Text file format converter
+    #fwupd                         # Firmware update tool (NixOS requires building this?????)
+    gptfdisk                      # Disk tools e.g. sgdisk, gdisk, cgdisk
+    #'intel-ucode'               # required for Intel Microcode update files to boot
+    inxi                          # CLI system information tool
+    jq                            # Command line JSON processor, depof: kubectl
+    libisoburn                    # xorriso ISO creation tools
+    logrotate                     # Rotates and compresses system logs
+    nix-prefetch                  # Utility to fetch git source to compute hashes
+    #'mkinitcpio-vt-colors'      # vt-colors, mkintcpio, find, xargs, gawk, grep
+    psmisc                        # Proc filesystem utilities e.g. killall
+    smartmontools                 # Monitoring tools for hard drives
+    squashfsTools                 # mksquashfs, unsquashfs
+    testdisk                      # Checks and undeletes partitions + photorec
+    tmux                          # Terminal multiplexer
+    tree                          # Simple dir listing app in tree form
+    usbutils                      # Tools for working with USB devices e.g. lsusb
+    yq                            # Command line YAML/XML/TOML processor
+
+    # Compression utilities
+    p7zip                         # Comman-line file archiver for 7zip format, depof: thunar
+    unrar                         # Unfree utility to uncompress RAR archives
+    unzip                         # Uncompress Zip archives
+    zip                           # Create zip archives
+
   ];
 
   # Configure /etc/bashrc to launch our installer automation 'clu'
