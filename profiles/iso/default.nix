64b155ba06a6f52f885e42bb5cdd9ee79c5322f1
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-03-29 11:12:01 -0600

    Working on separate packages file

--- profiles/iso/default.nix
+++ profiles/iso/default.nix
@@ -1,4 +1,4 @@
-# iso configuration
+# ISO default configuration
 # --------------------------------------------------------------------------------------------------
 # https://nixos.wiki/wiki/Creating_a_NixOS_live_CD
 #
@@ -11,6 +11,7 @@
     # I get a weird infinite recursion bug if I use ${pkgs} instead
     "${args.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
     ../../modules/nix.nix
+    ./packages.nix
   ];
 
   # ISO image configuration
@@ -19,46 +20,6 @@
   # Original example naming: "nixos-23.11.20240225.5bf1cad-x86_64-linux.iso"
   isoImage.isoBaseName = "nixos-installer";
   isoImage.isoName = "${config.isoImage.isoBaseName}-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}.iso";
-  #isoImage.includeSystemBuildDependencies = true; # includes source code; i just want packages
-  isoImage.storeContents = with pkgs; [
-    config.system.build.toplevel   # default ISO inclusion
-    
-    # Networking utilities
-    git                           # The fast distributed version control system
-    nfs-utils                     # Support programs for Network File Systems
-    wget                          # Retrieve files using HTTP, HTTPS, and FTP
-
-    # System utilities
-    efibootmgr                    # EFI Boot Manager
-    efivar                        # Tools to manipulate EFI variables
-    cdrtools                      # ISO tools e.g. isoinfo, mkisofs
-    ddrescue                      # GNU ddrescue, a data recovery tool
-    dos2unix                      # Text file format converter
-    #fwupd                         # Firmware update tool (NixOS requires building this?????)
-    gptfdisk                      # Disk tools e.g. sgdisk, gdisk, cgdisk
-    #'intel-ucode'               # required for Intel Microcode update files to boot
-    inxi                          # CLI system information tool
-    jq                            # Command line JSON processor, depof: kubectl
-    libisoburn                    # xorriso ISO creation tools
-    logrotate                     # Rotates and compresses system logs
-    nix-prefetch                  # Utility to fetch git source to compute hashes
-    #'mkinitcpio-vt-colors'      # vt-colors, mkintcpio, find, xargs, gawk, grep
-    psmisc                        # Proc filesystem utilities e.g. killall
-    smartmontools                 # Monitoring tools for hard drives
-    squashfsTools                 # mksquashfs, unsquashfs
-    testdisk                      # Checks and undeletes partitions + photorec
-    tmux                          # Terminal multiplexer
-    tree                          # Simple dir listing app in tree form
-    usbutils                      # Tools for working with USB devices e.g. lsusb
-    yq                            # Command line YAML/XML/TOML processor
-
-    # Compression utilities
-    p7zip                         # Comman-line file archiver for 7zip format, depof: thunar
-    unrar                         # Unfree utility to uncompress RAR archives
-    unzip                         # Uncompress Zip archives
-    zip                           # Create zip archives
-
-  ];
 
   # Configure /etc/bashrc to launch our installer automation 'clu'
   programs.bash.promptPluginInit = ''
