4f2c545112cacaabca83d03a554e56a1d50ad51e
phR0ze <prc451@gmail.com>
2024-03-30 17:36:39 -0600

    Working on live ISO

--- modules/boot/grub.nix
+++ modules/boot/grub.nix
@@ -4,18 +4,18 @@
 {
   # Configure default kernel modules
   #boot.loader = mkIf (args.settings.install) {
-  boot.loader = {
-    grub.enable = true;
-
-    # Defaults to '/boot' and only gets used if efiSupport is true
-    efi.efiSysMountPoint = "/boot";
-    grub.efiSupport = if args.settings.efi then true else false;
-
-    # i.e. EFI/BOOT/BOOTX64.efi
-    grub.efiInstallAsRemovable = if args.settings.efi then true else false;
-
-    # Configure or disable BIOS MBR boot support 
-    # Will be set with automation to, e.g. '/dev/sda' (MBR), or 'nodev' (EFI)
-    grub.device = args.settings.mbr;
-  };
+#  boot.loader = {
+#    grub.enable = true;
+#
+#    # Defaults to '/boot' and only gets used if efiSupport is true
+#    efi.efiSysMountPoint = "/boot";
+#    grub.efiSupport = if args.settings.efi then true else false;
+#
+#    # i.e. EFI/BOOT/BOOTX64.efi
+#    grub.efiInstallAsRemovable = if args.settings.efi then true else false;
+#
+#    # Configure or disable BIOS MBR boot support 
+#    # Will be set with automation to, e.g. '/dev/sda' (MBR), or 'nodev' (EFI)
+#    grub.device = args.settings.mbr;
+#  };
 }
