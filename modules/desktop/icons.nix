875908cc7df1daac275041ca225676a2182dd2ed
phR0ze <prc451@gmail.com>
2024-04-03 16:53:13 -0600

    Adding custom hicolor icons

--- modules/desktop/icons.nix
+++ modules/desktop/icons.nix
@@ -1,24 +1,24 @@
-# Background configuration
+# Icons configuration
 #
 # NixOS uses /run/current-system/sw/share in place of /usr/share
 #---------------------------------------------------------------------------------------------------
 { pkgs, ... }:
 let
   # 1. Build the package from the local files
-  backgroundsPackage = pkgs.runCommandLocal "backgrounds" {} ''
-    mkdir -p $out/share/backgrounds
-    cp ${../../include/usr/share/backgrounds}/* $out/share/backgrounds
+  iconsPackage = pkgs.runCommandLocal "icons" {} ''
+    mkdir -p $out/share/icons/hicolor
+    cp -r ${../../include/usr/share/icons/hicolor}/* $out/share/icons/hicolor
   '';
 in
 {
   # 2. Add the package to the /nix/store
   environment.systemPackages = [
-    backgroundsPackage
+    iconsPackage
   ];
 
   # 3. Link the package to the system path /run/current-system/sw 
   # - searches all packages that have paths matching the list and merge links them
   environment.pathsToLink = [
-    "/share/backgrounds"  # /run/current-system/sw/share/backgrounds
+    "/share/icons/hicolor"  # /run/current-system/sw/share/icons/hicolor
   ];
 }
