8b3afd29de1672bb49f5cbf6b4dbc98a97e3ae24
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-04-02 18:05:00 -0600

    Working on xfce background option

--- options/xfce/xfce4-desktop.nix
+++ options/xfce/xfce4-desktop.nix
@@ -44,19 +44,20 @@ let
       <?xml version="1.0" encoding="UTF-8"?>
 
       <channel name="xfce4-desktop" version="1.0">
-
         <property name="backdrop" type="empty">
           <property name="screen0" type="empty">
-            ${lib.concatMapStringsSep "\n" (x: ''      <property name="${x}" type="empty">
-              <property name="workspace0" type="empty">
-                <property name="color-style" type="int" value="0"/>
-                <property name="image-style" type="int" value="5"/>
-                <property name="last-image" type="string" value="${cfg.background}"/>
+            ${lib.concatMapStringsSep "\n" (x:
+            ''
+              <property name="${x}" type="empty">
+                <property name="workspace0" type="empty">
+                  <property name="color-style" type="int" value="0"/>
+                  <property name="image-style" type="int" value="5"/>
+                  <property name="last-image" type="string" value="${cfg.background}"/>
+                </property>
               </property>
-            </property>'') monitors}
+            '') monitors}
           </property>
         </property>
-
         <property name="desktop-icons" type="empty">
           <property name="icon-size" type="uint" value="48"/>
           <property name="show-thumbnails" type="bool" value="true"/>
