2778dc7c48fcac749a9fbe177c66d35b846f9c3d
phR0ze <prc451@gmail.com>
2024-04-03 13:17:38 -0600

    Working on custom bool conversion function

--- options/xfce/xfce4-desktop.nix
+++ options/xfce/xfce4-desktop.nix
@@ -4,7 +4,7 @@
 #---------------------------------------------------------------------------------------------------
 { options, config, lib, pkgs, args, ... }: with lib.types;
 let
-  #f = import ../../funcs;
+  f = pkgs.callPackage ../../funcs { inherit lib; };
   cfg = config.services.xserver.desktopManager.xfce.xfce4-desktop;
 
   monitors = [
@@ -65,10 +65,10 @@ let
           <property name="icon-size" type="uint" value="48"/>
           <property name="show-thumbnails" type="bool" value="true"/>
           <property name="file-icons" type="empty">
-            <property name="show-home" type="bool" value="${toString cfg.show-home}"/>
-            <property name="show-trash" type="bool" value="${toString cfg.show-trash}"/>
-            <property name="show-filesystem" type="bool" value="${toString cfg.show-filesystem}"/>
-            <property name="show-removable" type="bool" value="${toString cfg.show-removable}"/>
+            <property name="show-home" type="bool" value="${f.boolToStr cfg.show-home}"/>
+            <property name="show-trash" type="bool" value="${f.boolToStr cfg.show-trash}"/>
+            <property name="show-filesystem" type="bool" value="${f.boolToStr cfg.show-filesystem}"/>
+            <property name="show-removable" type="bool" value="${f.boolToStr cfg.show-removable}"/>
           </property>
         </property>
         <property name="last" type="empty">
