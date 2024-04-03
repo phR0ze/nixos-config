074a7856b9b1230d50353983a663412a897ea66c
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-04-03 16:31:58 -0600

    Shift over to use the nixos way of doing share

--- modules/desktop/backgrounds.nix
+++ modules/desktop/backgrounds.nix
@@ -3,20 +3,42 @@
 # NixOS uses /run/current-system/sw/share in place of /usr/share
 #---------------------------------------------------------------------------------------------------
 { pkgs, ... }:
-
 let
-  backgroundsPackage = pkgs.stdenvNoCC.mkDerivation {
-    name = "backgrounds";
-    src = ../../include/usr/share/backgrounds;
-    installPhase = ''
-      mkdir -p $out/share/backgrounds
-      cp $src/* $out/share/backgrounds
-    '';
-  };
+  backgroundsPackage = pkgs.runCommandLocal "backgrounds" {} ''
+    mkdir -p $out/share/backgrounds
+    cp ${../../include/usr/share/backgrounds}/* $out/share/backgrounds
+  '';
 in
 {
-  # Add it to the /run/current-system/sw/ directory, making it available to the system
+#  # Add a link to /run/current-system/sw/ to make the package available
+#  environment.pathsToLink = [
+#    "/share/icons/hicolor"      # searches all packages that have this path and links them
+#  ];
+
+  # Add the package to the /nix/store
   environment.systemPackages = [
     backgroundsPackage
   ];
 }
+
+#let
+#  backgroundsPackage = pkgs.stdenvNoCC.mkDerivation {
+#    name = "backgrounds";
+#    src = ../../include/usr/share/backgrounds;
+#    installPhase = ''
+#      mkdir -p $out/share/backgrounds
+#      cp $src/* $out/share/backgrounds
+#    '';
+#  };
+#in
+#{
+#  # Add a link to /run/current-system/sw/ to make the package available
+#  environment.pathsToLink = [
+#    "/share/icons/hicolor"      # searches all packages that have this path and links them
+#  ];
+#
+#  # Add the package to the /nix/store
+#  environment.systemPackages = [
+#    backgroundsPackage
+#  ];
+#}
