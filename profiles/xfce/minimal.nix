48a93caaa59815b5096f1f32c5ff03b7e1495417
phR0ze <prc451@gmail.com>
2024-04-03 20:00:38 -0600

    Working on xfce panel launchers

--- profiles/xfce/minimal.nix
+++ profiles/xfce/minimal.nix
@@ -22,6 +22,11 @@
       xfce.xfce4-panel.enable = true;
       xfce.xfce4-power-manager.enable = true;
       xfce.xfce4-desktop.background = lib.mkDefault "/run/current-system/sw/share/backgrounds/theater_curtains1.jpg";
+      xfce.xfce4-panel.launcher."terminal" = {
+        order = 1;
+        exec = "xfce4-terminal";
+        icon = "org.xfce.terminalemulator";
+      };
     };
     displayManager = {
       defaultSession = "xfce";
