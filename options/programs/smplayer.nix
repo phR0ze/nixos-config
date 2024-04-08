57a72ee68f825e970bc0b5fe3d0754a17878d906
phR0ze <prc451@gmail.com>
2024-04-07 19:51:14 -0600

    Working on smplayer themes

--- options/programs/smplayer.nix
+++ options/programs/smplayer.nix
@@ -2,7 +2,7 @@
 #
 # Adapted from https://gitlab.archlinux.org/archlinux/packaging/packages/smplayer-themes/-/blob/main/PKGBUILD
 #---------------------------------------------------------------------------------------------------
-{ options, config, lib, pkgs, wrapQtAppsHook, ... }: with lib.types;
+{ options, config, lib, pkgs, ... }: with lib.types;
 let
   cfg = config.programs.smplayer;
 
@@ -17,10 +17,10 @@ let
     # Include build time dependencies
     buildInputs = [
       pkgs.optipng
-      pkgs.libsForQt5.qtbase
+      pkgs.libsForQt5.qt5.qtbase
     ];
 
-    nativeBuildIInputs = [ wrapQtAppsHook ];
+    dontWrapQtApps = true;
 
     # Nix will make the current working directory the root of the unzipped package
     buildPhase = ''
