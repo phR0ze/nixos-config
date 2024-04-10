fc93b10b09da49b21b7e7d1458d7dfb286839df5
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-04-10 15:57:51 -0600

    Working logout

---
+++ options/programs/arcologout.nix
@@ -0,0 +1,53 @@
+# Arcologout options
+#
+#---------------------------------------------------------------------------------------------------
+{ options, config, lib, pkgs, ... }: with lib.types;
+let
+  cfg = config.programs.arcologout;
+
+  # Create the package adapted from Arch Linux
+  # https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=arcolinux-logout
+  arcologout = pkgs.stdenvNoCC.mkDerivation rec {
+    name = "arcologout";
+    version = "1.0.2";
+    src = pkgs.fetchFromGitHub {
+      owner = "phR0ze";
+      repo = "arcologout":
+      rev = "v${version}";
+      hash = "sha256-+O1fOL7qh/sHN1tEtIANt/+bEOCsjVmECLDBmSSQmHI=";
+    };
+
+    # Build time dependencies
+    nativeBuildInputs = [ ];
+
+    # Runtime dependencies
+    buildInputs = [ pkgs.python3 pkgs.python-cairo pkgs.python-gobject ];
+
+    buildPhase = ''
+      mkdir $out
+    '';
+
+    installPhase = ''
+      make PREFIX=$out install
+    '';
+  };
+
+in
+{
+  options = {
+    programs.arcologout = {
+      enable = lib.mkOption {
+        type = types.bool;
+        default = false;
+        description = lib.mdDoc "Install arcologout";
+      };
+    };
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf (cfg.enable) {
+      environment.systemPackages = with pkgs; [ arcologout ];
+      #files.all.".config/smplayer/themes".link = "${smplayer-themes}/share/smplayer/themes";
+    })
+  ];
+}
