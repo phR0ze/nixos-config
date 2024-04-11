ad8aa5489b0d785e006801c11c741612ab0bcd8e
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-04-11 16:25:21 -0600

    Adding default theater resolution

--- profiles/xfce/theater.nix
+++ profiles/xfce/theater.nix
@@ -9,8 +9,13 @@
     ./desktop.nix
   ];
 
-  #services.xserver.resolutions = [ { x = 1920; y = 1080; } ];
+  # Configure theater system resolution default
+  services.xserver.resolutions = [ { x = 1920; y = 1080; } ];
+
+  # Configure theater system background
   services.xserver.desktopManager.xfce.desktop.background = lib.mkOverride 500
     "/run/current-system/sw/share/backgrounds/theater_curtains1.jpg";
+
+  # Add additional theater package
   environment.systemPackages = with pkgs; [ ];
 }
