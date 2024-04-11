e0a954e7addaf41b8f7bd217e69a37915ab6ac9d
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-04-11 15:10:37 -0600

    Working on better slick theme

--- modules/desktop/xorg.nix
+++ modules/desktop/xorg.nix
@@ -40,36 +40,33 @@
 #        };
 #      };
 
-      lightdm = {
+      greeters.slick = {
         enable = true;
-        greeters.slick = {
-          enable = true;
-          theme.name = "Catppuccin-Frappe-Compact-Mauve-Dark";
-          theme.package = pkgs.catppuccin-gtk.override {
-            accents = ["mauve"];
-            size = "compact";
-            variant = "frappe";
-            tweaks = ["normal"];
-          };
-
-          iconTheme.name = "Papirus-Dark";
-          iconTheme.package = pkgs.catppuccin-papirus-folders.override {
-            flavor = "frappe";
-            accent = "mauve";
-          };
-
-          font.name = "NotoSans Nerd Font Regular";
-          font.package = pkgs.nerdfonts.override {fonts = ["Noto"];};
-
-          cursorTheme.package = pkgs.catppuccin-cursors.frappeDark;
-          cursorTheme.name = "Catppuccin-Frappe-Dark-Cursors";
-          cursorTheme.size = 32;
-
-          extraConfig = ''
-            background=#ca9ee6
-            enable-hidpi=on
-          '';
+        theme.name = "Catppuccin-Frappe-Compact-Mauve-Dark";
+        theme.package = pkgs.catppuccin-gtk.override {
+          accents = ["mauve"];
+          size = "compact";
+          variant = "frappe";
+          tweaks = ["normal"];
         };
+
+        iconTheme.name = "Papirus-Dark";
+        iconTheme.package = pkgs.catppuccin-papirus-folders.override {
+          flavor = "frappe";
+          accent = "mauve";
+        };
+
+        font.name = "NotoSans Nerd Font Regular";
+        font.package = pkgs.nerdfonts.override {fonts = ["Noto"];};
+
+        cursorTheme.package = pkgs.catppuccin-cursors.frappeDark;
+        cursorTheme.name = "Catppuccin-Frappe-Dark-Cursors";
+        cursorTheme.size = 32;
+
+        extraConfig = ''
+          background=#ca9ee6
+          enable-hidpi=on
+        '';
       };
       # Conditionally autologin based on install settings
       #autoLogin.enable = args.settings.autologin;
