91f2ff608abe18ee1b771907e936e32d92536c12
phR0ze <prc451@gmail.com>
2024-04-13 15:47:38 -0600

    Working on adding nvtop

--- modules/hardware/video.nix
+++ modules/hardware/video.nix
@@ -93,7 +93,7 @@
     pciutils                            # Collection of utilities for inspecting and manipulating PCI devices
     vdpauinfo                           # Tool to query the Video Decode and Presentation API (VDPAU)
     nvtop                               # A (h)top like task monitor for AMD, Adreno, Intel and NVIDIA
-    #.nvtopPackages.full                  # A (h)top like task monitor for AMD, Adreno, Intel and NVIDIA
+    radeontop                           # Top-like tool for viewing AMD Radeon GPU utilization
   ];
 
   # Video drivers to be tried in order until one that supports your card is found
