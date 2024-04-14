acd3532d4912b270ba32091188dbe68b7db63c69
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-04-14 10:31:30 -0600

    Working on lightdm default background

--- profiles/xfce/theater.nix
+++ profiles/xfce/theater.nix
@@ -16,6 +16,9 @@
   services.xserver.desktopManager.xfce.desktop.background = lib.mkOverride 500
     "/run/current-system/sw/share/backgrounds/theater_curtains1.jpg";
 
+  # Todo convert backgrounds into a pkg that can be called and made reusable
+  #services.xserver.displayManager.lightdm.background = "${backgroundsPackage}/share/backgrounds/sector-8_1600x900.jpg";
+
   # Add additional theater package
   environment.systemPackages = with pkgs; [ ];
 }
