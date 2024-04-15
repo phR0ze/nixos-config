a5f49fe240cffcb6af1a0c03616bc2f7ba9bffd6
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-04-15 12:46:44 -0600

    Use opt.nix rather than pkg.nix

--- modules/desktop/backgrounds/default.nix
+++ modules/desktop/backgrounds/default.nix
@@ -1,22 +1,9 @@
-# Backgrounds module
+# Backgrounds package
 #
-# NixOS uses /run/current-system/sw/share in place of /usr/share
 #---------------------------------------------------------------------------------------------------
 { pkgs, ... }:
-let
-  backgrounds = pkgs.callPackage ./pkg.nix { };
 
-in
-{
-
-  # 2. Add the package to the /nix/store
-  environment.systemPackages = [
-    backgrounds
-  ];
-
-  # 3. Link the package to the system path /run/current-system/sw 
-  # - searches all packages that have paths matching the list and merge links them
-  environment.pathsToLink = [
-    "/share/backgrounds"  # /run/current-system/sw/share/backgrounds
-  ];
-}
+pkgs.runCommandLocal "backgrounds" {} ''
+  mkdir -p $out/share/backgrounds
+  cp ${../../../include/usr/share/backgrounds}/* $out/share/backgrounds
+''
