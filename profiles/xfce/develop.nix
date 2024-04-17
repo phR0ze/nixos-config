dc4ec8d28dfe0469fe7d2929bcb7c51b5b1db36c
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-04-17 09:19:31 -0600

    Adding captive browser idea for later

--- profiles/xfce/develop.nix
+++ profiles/xfce/develop.nix
@@ -13,4 +13,12 @@
 
   # Additional programs and services
   services.barriers.enable = true;      # Enable the barrier server and client
+
+  # Slick captive portal solutions for hotels etc...
+#  programs = {
+#    captive-browser = {
+#      enable = true;
+#      interface = config.lib._custom_.wirelessInterface;
+#    };
+#  };
 }
