e7d2d955a21b9cf7db3517ae224a66d590493434
phR0ze <prc451@gmail.com>
2024-04-17 11:57:28 -0600

    Conditionally add the kodi launcher

--- profiles/xfce/theater.nix
+++ profiles/xfce/theater.nix
@@ -34,21 +34,6 @@ in
   services.xserver.displayManager.lightdm.background = lib.mkOverride 500
     "${backgrounds}/share/backgrounds/theater_curtains1.jpg";
 
-  # Set xfce launchers
-  services.xserver.desktopManager.xfce.panel.launchers = [
-    { name = "Xfce4 Terminal"; exec = "xfce4-terminal"; icon = "org.xfce.terminalemulator"; }
-    { name = "Thunar"; exec = "exo-open --launch FileManager"; icon = "org.xfce.thunar"; }
-    { name = "XnviewMP"; exec = "xnviewmp"; icon = "xnviewmp"; }
-    { name = "Kodi"; exec = "kodi"; icon = "kodxnviewmpi"; }
-    { name = "SMPlayer"; exec = "smplayer"; icon = "smplayer"; }
-    { name = "HandBrake"; exec = "ghb"; icon = "fr.handbrake.ghb"; }
-    { name = "VLC Media Player"; exec = "vlc"; icon = "vlc"; }
-    { name = "FileZilla"; exec = "filezilla"; icon = "filezilla"; }
-    { name = "Firefox"; exec = "firefox"; icon = "firefox"; }
-    { name = "LibreOffice Calc"; exec = "libreoffice --calc"; icon = "libreoffice-calc"; }
-    { name = "LibreOffice Writer"; exec = "libreoffice --writer"; icon = "libreoffice-writer"; }
-  ];
-
   # Add additional theater package
   environment.systemPackages = with pkgs; [ ];
 }
