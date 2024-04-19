49aae83d1164afdb51661f1a441e1775151d50d6
phR0ze <prc451@gmail.com>
2024-04-19 16:41:45 -0600

    Moving vscode to develop profile

--- modules/development/vscode/extensions.nix
+++ modules/development/vscode/extensions.nix
@@ -7,11 +7,13 @@
     # General extensions
     # ----------------------------------------------------------------------------------------
     tamasfe.even-better-toml    # Even Better TOML
+    anseki.vscode-color         # GUI color picker to generate color codes
 
     golang.go
 
     # Rust extensions
     # ----------------------------------------------------------------------------------------
+    rust-lang.rust-analyzer     # Rust language support, code completion, go to definition etc...
     vadimcn.vscode-lldb         # A native debugger powered by LLDB for C++, Rust and other compiled languages
 
 #  ] ++ pkgs.vscode-utils.extensionsFromVsCodeMarketplce [
