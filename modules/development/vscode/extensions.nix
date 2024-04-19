194ab99d4f9f5605a10c335abf96aebfdc75140a
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-04-19 16:24:38 -0600

    Adding vscode extensions

--- modules/development/vscode/extensions.nix
+++ modules/development/vscode/extensions.nix
@@ -3,6 +3,18 @@
 { config, lib, pkgs, args, settingsFilePath, ... }:
 {
   programs.vscode.extensions = with pkgs.vscode-extensions; [
+
+    # General extensions
+    # ----------------------------------------------------------------------------------------
+    tamasfe.even-better-toml    # Even Better TOML
+
     golang.go
+
+    # Rust extensions
+    # ----------------------------------------------------------------------------------------
+    vadimcn.vscode-lldb         # A native debugger powered by LLDB for C++, Rust and other compiled languages
+
+  ] ++ pkgs.vscode-utils.extensionsFromVsCodeMarketplce [
+
   ];
 }
