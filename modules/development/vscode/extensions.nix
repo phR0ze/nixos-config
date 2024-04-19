4e70a77742fe2145910702775654e6c91a40894e
phR0ze <prc451@gmail.com>
2024-04-19 17:14:39 -0600

    Working on vscode extensions

--- modules/development/vscode/extensions.nix
+++ modules/development/vscode/extensions.nix
@@ -6,27 +6,28 @@
 
     # General extensions
     # ----------------------------------------------------------------------------------------
-    tamasfe.even-better-toml          # Even Better TOML
-    github.copilot                    # Github Copilot uses OpenAI Codex to suggest code
-    github.copilot-chat               # Copilot companion extension for chat interface
-    golang.go                         # Google official Golang support
-    emmanuelbeziat.vscode-great-icons # Awesome icon pack for vscode
-    vscodevim.vim                     # Essential vim syntax in vscode
+    tamasfe.even-better-toml            # Even Better TOML
+    github.copilot                      # Github Copilot uses OpenAI Codex to suggest code
+    github.copilot-chat                 # Copilot companion extension for chat interface
+    golang.go                           # Google official Golang support
+    emmanuelbeziat.vscode-great-icons   # Awesome icon pack for vscode
+    vscodevim.vim                       # Essential vim syntax in vscode
+    ms-vscode-remote.remote-containers  # Open and folder or repo inside a Docker container
 
     # Dart extensions
     # ----------------------------------------------------------------------------------------
-    dart-code.flutter                 # Official flutter mobile apps support
-    dart-code.dart-code               # Dart language support and debugger for vscode
+    dart-code.flutter                   # Official flutter mobile apps support
+    dart-code.dart-code                 # Dart language support and debugger for vscode
 
     # Rust extensions
     # ----------------------------------------------------------------------------------------
-    rust-lang.rust-analyzer           # Rust language support, code completion, go to definition etc...
-    vadimcn.vscode-lldb               # A native debugger powered by LLDB for C++, Rust and other compiled languages
-    serayuzgur.crates                 # Simplify dependency management in Rust
+    rust-lang.rust-analyzer             # Rust language support, code completion, go to definition etc...
+    vadimcn.vscode-lldb                 # A native debugger powered by LLDB for C++, Rust and other compiled languages
+    serayuzgur.crates                   # Simplify dependency management in Rust
 
   ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
     {
-      name = "vscode-color";          # GUI color picker to generate color codes
+      name = "vscode-color";            # GUI color picker to generate color codes
       publisher = "anseki";
       version = "0.4.5";
       sha256 = "sha256-xclKrAqa/00xmlfqgIi0cPNyzDI6HFc+bz2kpm4d1AY=";
@@ -38,7 +39,7 @@
       sha256 = "sha256-IgfcIRF54JXm9l2vVjf7lFJOVSI0CDgDjQT+Hw6FO4Q=";
     }
     {
-      name = "vscode-remove-comments"; # Remove all comments from the current selection or the whole doc
+      name = "vscode-remove-comments";  # Remove all comments from the current selection or the whole doc
       publisher = "rioj7";
       version = "1.8.0";
       sha256 = "sha256-eG5cj1ygGeOI/fttmJJbqFrFNjDUOKbqNOS2Ai+tNYI=";
