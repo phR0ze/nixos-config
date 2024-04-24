0ae76a6f182b34b1bd2dd67eabecf0d657367ac6
phR0ze <prc451@gmail.com>
2024-04-24 15:59:02 -0600

    Allow smplayer and xnviewmp to retain their settings after an update

--- modules/multimedia/xnviewmp.nix
+++ modules/multimedia/xnviewmp.nix
@@ -60,7 +60,7 @@ in
   config = lib.mkMerge [
     (lib.mkIf (cfg.enable) {
       environment.systemPackages = with pkgs; [ xnviewmp ];
-      files.all.".config/xnviewmp/xnview.ini".copy = ../../include/home/.config/xnviewmp/xnview.ini;
+      files.all.".config/xnviewmp/xnview.ini".weakCopy = ../../include/home/.config/xnviewmp/xnview.ini;
     })
   ];
 }
