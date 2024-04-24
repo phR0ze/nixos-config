f8de4c76ec972d475926fe2f77395e785d76a787
phR0ze <prc451@gmail.com>
2024-04-24 16:53:14 -0600

    Enable barrier only for workstation

--- modules/desktop/x11/develop.nix
+++ modules/desktop/x11/develop.nix
@@ -6,11 +6,7 @@
 {
   imports = [
     ../../development/vscode
-    ../../services/barrier.nix
   ];
-
-  # Additional programs and services
-  services.barriers.enable = true;      # Enable the barrier server and client
 
   environment.systemPackages = with pkgs; [
     chromium                            # An open source web browser from Google
