04d6187a18f9abf7916fe1050812adfc82799580
phR0ze <prc451@gmail.com>
2024-04-26 15:32:00 -0600

    Collapse x11 profiles back into generic

--- profiles/generic/develop.nix
+++ profiles/generic/develop.nix
@@ -7,10 +7,17 @@
 {
   imports = [
     ./desktop.nix
+    ../../modules/development/vscode
     ../../modules/desktop/x11/develop.nix
   ];
 
+  development.rust.enable = true;
+
   services.xserver.desktopManager.xfce.menu.overrides = [
     { source = "${pkgs.vscode}/share/applications/code.desktop"; categories = "Development"; }
+  ];
+
+  environment.systemPackages = with pkgs; [
+    chromium                            # An open source web browser from Google
   ];
 }
