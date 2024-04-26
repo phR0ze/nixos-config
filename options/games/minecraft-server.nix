67bb8142c218567f80f831f7e60be258da326893
phR0ze <prc451@gmail.com>
2024-04-26 15:05:31 -0600

    Adding minecraft jvm options

--- options/games/minecraft-server.nix
+++ options/games/minecraft-server.nix
@@ -8,9 +8,33 @@ in
 {
   config = lib.mkIf (cfg.enable) {
     cfg.eula = true;            # means agreeing to Mojang's EULA: https://account.mojang.com/documents/minecraft_eula
-    cfg.jvmOpts = "";
 
-    #cfg.declarative = true;     # enables the serverProperties to take effect
+    # JVM configuration
+    # https://github.com/brucethemoose/Minecraft-Performance-Flags-Benchmarks?tab=readme-ov-file#server-g1gc
+    cfg.jvmOpts = lib.concatStringsSep " " [
+      "-Xms4G -Xmx4G"                         # always bound the amount of memory allowed to the JVM
+      "-XX:+UseG1GC"                          # use the G1GC garbabe collector
+      "-XX:MaxGCPauseMillis=130"
+      "-XX:+UnlockExperimentalVMOptions"
+      "-XX:+DisableExplicitGC"
+      "-XX:+AlwaysPreTouch"
+      "-XX:G1NewSizePercent=28"
+      "-XX:G1HeapRegionSize=16M"
+      "-XX:G1ReservePercent=20"
+      "-XX:G1MixedGCCountTarget=3"
+      "-XX:InitiatingHeapOccupancyPercent=10"
+      "-XX:G1MixedGCLiveThresholdPercent=90"
+      "-XX:G1RSetUpdatingPauseTimePercent=0"
+      "-XX:SurvivorRatio=32"
+      "-XX:MaxTenuringThreshold=1"
+      "-XX:G1SATBBufferEnqueueingThresholdPercent=30"
+      "-XX:G1ConcMarkStepDurationMillis=5"
+      "-XX:G1ConcRSHotCardLimit=16"
+      "-XX:G1ConcRefinementServiceIntervalMillis=150"
+    ];
+
+    # Enable serverProperties to take effect
+    #cfg.declarative = true;
     #cfg.serverProperties {
     #  gamemode = "survival";
     #  difficulty = 3;
