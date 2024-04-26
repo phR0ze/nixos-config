f43d45f498fff65352f96f71a50ebb1e54164553
phR0ze <prc451@gmail.com>
2024-04-25 23:53:10 -0600

    Refactoring as always imported options

--- modules/desktop/x11/develop.nix
+++ modules/desktop/x11/develop.nix
@@ -8,18 +8,9 @@
     ../../development/vscode
   ];
 
-  # Add cargo's bin to the environment 
-  environment.extraInit = ''
-    export PATH="$HOME/.cargo/bin:$PATH"
-  '';
+  development.rust.enable = true;
 
   environment.systemPackages = with pkgs; [
     chromium                            # An open source web browser from Google
-    clang                               # A C language family frontend for LLVM
-    lldb                                # Next gen high-performance debugger for Rust
-    llvm                                # Compiler infrastructure
-    rustup                              # Rust installer
-    cargo                               # Rust project dependency management tooling
-    rustfmt                             # Rust tool for formatting rust code
   ];
 }
--- options/development/default.nix
+++ options/development/default.nix
@@ -3,7 +3,6 @@
 { ... }:
 {
   imports = [
-    ./files
-    ./development
+    ./rust.nix
   ];
 }
