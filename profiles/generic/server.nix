dc44d05b52e3260c5b51ee178193af4ead4670dc
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-04-27 15:40:07 -0600

    Working on virt-manager

--- profiles/generic/server.nix
+++ profiles/generic/server.nix
@@ -9,13 +9,8 @@
     ./desktop.nix
   ];
 
-  development.rust.enable = true;
-  virtualisation.boxes.enable = true;
-  services.minecraft-server.enable = true;
-
-  services.xserver.desktopManager.xfce.menu.overrides = [
-    { source = "${pkgs.vscode}/share/applications/code.desktop"; categories = "Development"; }
-  ];
+  virtualization.virt-manager.enable = true;
+  #services.minecraft-server.enable = true;
 
   environment.systemPackages = with pkgs; [
     chromium                            # An open source web browser from Google
