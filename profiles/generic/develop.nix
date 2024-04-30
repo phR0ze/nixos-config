20b11851b9631bdc8a9b3d1d5a20ff82137e0969
phR0ze <prc451@gmail.com>
2024-04-30 15:05:06 -0600

    Adding golang tooling

--- profiles/generic/develop.nix
+++ profiles/generic/develop.nix
@@ -20,5 +20,8 @@
     chromium                            # An open source web browser from Google
     gnumake                             # A tool to control the generation of non-source files from sources
     google-cloud-sdk                    # Tools for the google cloud platform
+    go                                  # The Go programming language
+    go-bindata                          # Golang code generation utility for embedding binary data in Go programs
+    golangci-lint                       # Golang CI linting tool
   ];
 }
