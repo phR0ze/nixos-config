138f9d200753ab3101d9b14ca83a1a5c1c30a434
phR0ze <prc451@gmail.com>
2024-05-03 14:16:10 -0600

    Testing barrier

--- profiles/dell-xps-13-9310/develop.nix
+++ profiles/dell-xps-13-9310/develop.nix
@@ -12,9 +12,7 @@
   hardware.intel-graphics.enable = true;
   services.xserver.xft.dpi = 115;
   services.x11vnc.enable = lib.mkForce false;
-
   virtualization.virt-manager.enable = true;
 
-  environment.systemPackages = with pkgs; [
-  ];
+  services.barrierc.enable = true;
 }
