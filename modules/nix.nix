6986652d95b957ae43796b3cff464cf21afb7105
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-05-09 15:52:45 -0600

    Testing switch

--- modules/nix.nix
+++ modules/nix.nix
@@ -3,6 +3,7 @@
 { config, lib, pkgs, args, ... }:
 {
   # Set the git revision to be used in the system version `clu list versions`
+  #shortRev = builtins.substring 0 8 rev;
   system.configurationRevision = lib.mkIf (args.self ? rev) args.self.rev;
 
   nix = {
