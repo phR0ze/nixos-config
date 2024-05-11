5457fb5a566cc10af96182b7bbecb5e111c311b3
phR0ze <prc451@gmail.com>
2024-05-11 15:34:26 -0600

    Add the ability to build test vm easily

--- modules/services/systemd.nix
+++ modules/services/systemd.nix
@@ -4,7 +4,7 @@
 # - Shutdown your system with: shutdown
 # - Reboot your system with: reboot
 #---------------------------------------------------------------------------------------------------
-{ ... }:
+{ lib, ... }:
 {
   # Logind configuration
   # - Defaults were changed here https://github.com/NixOS/nixpkgs/pull/16021
@@ -24,5 +24,5 @@
   # Timesyncd configuration
   # Defaults are fine
   # ------------------------------------------------------------------------------------------------
-  services.timesyncd.enable = true;
+  services.timesyncd.enable = lib.mkForce true;
 }
