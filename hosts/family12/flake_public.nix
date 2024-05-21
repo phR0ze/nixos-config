1dd077e9a73bfd53114221cacc5084115899fb97
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-05-20 22:56:08 -0600

    Adding additional host info

---
+++ hosts/family12/flake_public.nix
@@ -0,0 +1,21 @@
+{
+  # Public clu installer options
+  # ------------------------------------------------------------------------------------------------
+  hostname = "family12";          # hostname to use for the install
+  static_ip = "192.168.1.12/24";  # static ip to use if set e.g. 192.168.1.2/24
+  gateway = "192.168.1.1";        # default gateway to use for static ip addresses
+  bluetooth = false;              # flag to control bluetooth enablement
+
+  profile = "generic/develop";    # pre-defined configurations in path './profiles' selection
+  nfs_shares = true;              # enable the nfs client shares for this system
+  autologin = false;              # automatically log the user in after boot when true
+  
+  # Configuration set via automation in the clu installer
+  # ------------------------------------------------------------------------------------------------
+  efi = true;                     # EFI system boot type, default "false"
+  mbr = "nodev";                  # MBR system boot device, default "nodev"
+  system = "x86_64-linux";        # system architecture to use
+  timezone = "America/Boise";     # time-zone selection
+  locale = "en_US.UTF-8";         # locale selection
+  comment = "";                   # Placeholder for injected nixos-config comment
+}
