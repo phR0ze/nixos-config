d32f697bac1d79df53bc1da69719e7e524f17300
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-05-20 22:20:35 -0600

    Splitting out flake options to private and public

--- flake_public.nix
+++ flake_public.nix
@@ -1,13 +1,6 @@
 {
-  # Configuration set by user selections in the clu installer
+  # Public clu installer options
   # ------------------------------------------------------------------------------------------------
-  fullname = "admin";             # user's full name to use for general purposes
-  email = "nixos@nixos.org";      # email to use for general configuration
-  username = "admin";             # initial admin user to create during install
-  userpass = "admin";             # admin user password securely entered during boot
-  git_user = "admin";             # username to use for github, gitlab or other git tools
-  git_email = "nixos@nixos.org";  # email to use for github, gitlab or other git tools
-
   hostname = "nixos";             # hostname to use for the install
   static_ip = "";                 # static ip to use if set e.g. 192.168.1.2/24
   gateway = "";                   # default gateway to use for static ip addresses
