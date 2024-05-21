73ba36c6c47190a7505fce22d023837d7fe3feec
phR0ze <prc451@gmail.com>
2024-05-20 22:11:25 -0600

    Switch out xfce-appfinder for dmenu

--- options/utils/dmenu.nix
+++ options/utils/dmenu.nix
@@ -1,19 +1,48 @@
 # dmenu configuration
 #
+# ### Notes:
+# - alterntive to the default xfce4-appfinder
 # --------------------------------------------------------------------------------------------------
 { config, lib, pkgs, args, ... }: with lib.types;
 let
   cfg = config.programs.dmenu;
 
+  runDesktopFilePackage = pkgs.runCommandLocal "run" {} ''
+    mkdir -p $out/share/applications
+    cat > $out/share/applications/run.desktop <<EOF
+    [Desktop Entry]
+    Version=1.1
+    Type=Application
+    Name=Run...
+    Icon=applications-other
+    Exec=${cfg.run}
+    Actions=
+    Categories=X-XFCE;X-Xfce-Toplevel;
+    EOF
+  '';
 in
 {
   options = {
     programs.dmenu = {
       enable = lib.mkEnableOption "Install and configure dmenu";
+
+      run = lib.mkOption {
+        type = types.str;
+        default = "dmenu_run -fn -misc-fixed-*-*-*-*-20-200-*-*-*-*-*-*  -i -nb '#000000' -nf '#efefef' -sf '#000000' -sb '#3cb0fd'";
+        description = lib.mdDoc "Run command to use";
+      };
     };
   };
  
   config = lib.mkIf (cfg.enable) {
-    environment.systemPackages = with pkgs; [ dmenu ];
+    environment.systemPackages = with pkgs; [
+      dmenu
+      runDesktopFilePackage
+    ];
+
+    # Trigger linking the package to the system path /run/current-system/sw 
+    environment.pathsToLink = [
+      "/share/applications"
+    ];
   };
 }
