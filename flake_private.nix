318869db5b1a73fb9977d966d691824d4c578af3
phR0ze <prc451@gmail.com>
2024-05-20 22:20:35 -0600

    Splitting out flake options to private and public

---
+++ flake_private.nix
@@ -0,0 +1,10 @@
+{
+  # Private clu installer options
+  # ------------------------------------------------------------------------------------------------
+  fullname = "admin";             # user's full name to use for general purposes
+  email = "nixos@nixos.org";      # email to use for general configuration
+  username = "admin";             # initial admin user to create during install
+  userpass = "admin";             # admin user password securely entered during boot
+  git_user = "admin";             # username to use for github, gitlab or other git tools
+  git_email = "nixos@nixos.org";  # email to use for github, gitlab or other git tools
+}
