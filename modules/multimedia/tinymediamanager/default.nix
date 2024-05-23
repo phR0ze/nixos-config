ae7f19110927eabd154dccb82144e93324530ba1
phR0ze <prc451@gmail.com>
2024-05-23 16:08:32 -0600

    Adding tiny media manager to server profile

--- modules/multimedia/tinymediamanager/default.nix
+++ modules/multimedia/tinymediamanager/default.nix
@@ -1,7 +1,14 @@
 # Tiny media manager
+# - https://gitlab.com/tinyMediaManager/tinyMediaManager
+# - https://www.reddit.com/r/tinyMediaManager/comments/14yzalj/libmediainfo/
+#
+# ### License changes
+# - Releae 4.1 changed the license to make FREE and PRO versions
+# - https://gitlab.com/tinyMediaManager/tinyMediaManager/-/releases/tinyMediaManager-4.1
 #---------------------------------------------------------------------------------------------------
 { stdenv, lib, pkgs, fetchurl,libmediainfo,libzen,jre }:
 let
+  # Build a wrapper script that will configure an environment for Tiny Media Manager to run in
   tmm-bin = pkgs.writers.writeDash "tinymediamanager" ''
     TMMDIR=$(dirname $(readlink -f $0))/../lib/tmm
     tmp=$(mktemp -d) # workaround for unwriteable base directory
@@ -17,12 +24,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "tinymediamanager";
-  version = "3.1.6";
+  version = "3.1.10";
 
   src = fetchurl {
-    #url = "http://release.tinymediamanager.org/v3/dist/tmm_${version}_linux.tar.gz";
-    url = "https://archive.org/download/tmm_linux.tar/tmm_${version}_linux.tar.gz";
-    sha256 = "06h0q52f0q4aq600nqzjkgrbkwxa2gnpbbn3nbfhni550bdgvf40";
+    url = "https://gitlab.com/tinyMediaManager/tinyMediaManager/uploads/1e473ce8af04196db2c7097aad475d03/tmm_${version}_linux.tar.gz";
+    sha256 = "sha256-Ox1uGHDHlleN5DlBKTsCbBxmJwr1n7KZkC1OSbtYrjs=";
   };
   sourceRoot = ".";
 
@@ -33,9 +39,10 @@ stdenv.mkDerivation rec {
     cp -r . $out/lib/tmm
   '';
 
+  # NixOS license values can be found in nixpkgs/lib/licenses.nix
   meta = {
-    homepage = http://users.eastlink.ca/~doiron/bin2iso/ ;
-    description = "converts bin+cue to iso";
-    license = lib.licenses.gpl3;
+    homepage = https://www.tinymediamanager.org/;
+    description = "tinyMediaManager is a media management tool that creats NFO files for use with Kodi";
+    license = lib.licenses.asl20;
   };
 }
---
+++ options/multimedia/tinymediamanager.nix
@@ -0,0 +1,40 @@
+# Tiny media manager options
+#---------------------------------------------------------------------------------------------------
+{ config, lib, pkgs, ... }: with lib.types;
+let
+  cfg = config.programs.tinyMediaManager;
+  tinymediamanager = pkgs.callPackage ../../modules/multimedia/tinymediamanager/default.nix { };
+
+  tmmDesktopFilePackage = pkgs.runCommandLocal "tinyMediaManager" {} ''
+    mkdir -p $out/share/applications
+    cat > $out/share/applications/tinymediamanager.desktop <<EOF
+    [Desktop Entry]
+    Version=1.1
+    Type=Application
+    Name=tinyMediaManager
+    Icon=${tinymediamanager}/lib/tmm/tmm.png
+    Exec=tinymediamanager
+    Actions=
+    Categories=Multimedia;
+    EOF
+  '';
+in
+{
+  options = {
+    programs.tinyMediaManager = {
+      enable = lib.mkEnableOption "Install and configure Tiny Media Manager";
+    };
+  };
+
+  config = lib.mkIf (cfg.enable) {
+    environment.systemPackages = [
+      tinymediamanager
+      tmmDesktopFilePackage
+    ];
+
+    # Trigger linking the package to the system path /run/current-system/sw 
+    environment.pathsToLink = [
+      "/share/applications"
+    ];
+  };
+}
