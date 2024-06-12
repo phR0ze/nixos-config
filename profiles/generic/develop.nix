fda96363297e2da0ad9c3fb5a64f5c29418f3828
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-06-12 11:11:43 -0600

    Adding python3 to generic dev

--- profiles/generic/develop.nix
+++ profiles/generic/develop.nix
@@ -19,14 +19,17 @@
     chromium                            # An open source web browser from Google
     google-cloud-sdk                    # Tools for the google cloud platform
 
+    # Standard development
+    gnumake                             # A tool to control the generation of non-source files from sources
+    pkg-config                          # At tool that allows packages to find out information about other packages
+
     # Golang development
     go                                  # The Go programming language
     go-bindata                          # Golang code generation utility for embedding binary data in Go programs
     golangci-lint                       # Golang CI linting tool
 
-    # Standard development
-    gnumake                             # A tool to control the generation of non-source files from sources
-    pkg-config                          # At tool that allows packages to find out information about other packages
+    # Python
+    python3                             # Python 3 runtime
   ];
 
 }
