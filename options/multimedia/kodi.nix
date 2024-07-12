980c7b187cecafd154b976eeed59a0b42b60d082
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-07-11 19:09:38 -0600

    Adding kodi firewall option

---
+++ options/multimedia/kodi.nix
@@ -0,0 +1,33 @@
+# kodi
+# A software media player and entertainment hub for digital media
+#---------------------------------------------------------------------------------------------------
+{ config, lib, pkgs, ... }: with lib.types;
+let
+  cfg = config.programs.kodi;
+
+in
+{
+  options = {
+    programs.kodi = {
+      enable = lib.mkEnableOption "Install and configure kodi";
+      port = lib.mkOption {
+        type = types.port;
+        default = 8080;
+        description = lib.mdDoc "Port to use for remote control.";
+      };
+      remoteControlHTTP = lib.mkOption {
+        type = types.bool;
+        default = false;
+        description = lib.mdDoc "Enable HTTP remote control.";
+      };
+    };
+  };
+
+  config = lib.mkIf (cfg.enable) {
+    environment.systemPackages = with pkgs; [ kodi ];
+
+    # Allow remote control through the firewall
+    # View rules with: sudo iptables -S
+    networking.firewall.allowedTCPPorts = lib.optional cfg.remoteControlHTTP "${cfg.port}";
+  };
+}
