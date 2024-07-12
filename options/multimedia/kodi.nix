a37804bab9d34c007f181f963a1ee1a0d1149ac5
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-07-11 19:15:51 -0600

    Use integer directly

--- options/multimedia/kodi.nix
+++ options/multimedia/kodi.nix
@@ -28,6 +28,6 @@ in
 
     # Allow remote control through the firewall
     # View rules with: sudo iptables -S
-    networking.firewall.allowedTCPPorts = lib.optional cfg.remoteControlHTTP "${cfg.port}";
+    networking.firewall.allowedTCPPorts = lib.optional cfg.remoteControlHTTP cfg.port;
   };
 }
