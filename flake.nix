8b574999056f3dff32e5f1cfc7208eb728646a18
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-07-23 15:02:43 -0600

    Building in a latest unstable path

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
   # After inputs are resolved they are passed to the outputs function and map to the explicit and 
   # implicit arguments as defined by the outputs function.
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";          # pinned pseudo stable
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable"; # latest unstable for upgrades
   };
 
   # ### Implicit arguments
@@ -24,7 +24,7 @@
   # Although it is nice to gather all implicit arguments together this means to use them without the 
   # dot notation would require an 'inherit (inputs) nixpkgs' to bring them into scope. Another option 
   # is to just call them out explicitly as required named arguments which does this scoping for you.
-  outputs = { self, nixpkgs, ... }@inputs: let
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: let
 
     # Configurable system options
     # ----------------------------------------------------------------------------------------------
@@ -39,6 +39,12 @@
     # * [lookup-paths](https://nix.dev/tutorials/nix-language.html#lookup-paths)
     # * [Override nixpkgs](https://discourse.nixos.org/t/allowunfree-predicate-does-not-apply-to-self-packages/21734/6)
     # ----------------------------------------------------------------------------------------------
+    pkgs-unstable = import nixpkgs-unstable {
+      system = settings.system;
+      config.allowUnfree = true;
+      config.allowUnfreePredicate = _: true;
+    };
+
     pkgs = import nixpkgs {
       system = settings.system;
       config.allowUnfree = true;
@@ -47,15 +53,17 @@
         #"qtwebkit-5.212.0-alpha4"
       ];
 
-#      overlays = [
-#        (final: prev: {
-#          prismlauncher = prev.prismlauncher.override (p: {
-#            prismlauncher-unwrapped = p.prismlauncher-unwrapped.overrideAttrs (o: {
-#              patches = (o.patches or [ ]) ++ [ ./patches/prismlauncher/offline.patch ];
-#            });
-#          });
-#        })
-#      ];
+      # Modify the set of packages to be used for installs
+      # --------------------------------------------------------------------------------------------
+      # 'self' is the set of packages before any modifications
+      # 'super' is the super set of packages after being modified
+      overlays = [
+
+        # Upgrade select packages to the latest unstable bits
+        (self: super: {
+          vscode = pkgs-unstable.vscode;
+        })
+      ];
     };
 
     # Import all custom functions to be use throughout
