e9d48b8995dce827b0573ae1b2f049afa44a0eb2
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-11-13 20:08:26 -0700

    Dropping hosts

--- profiles/generic/desktop.nix
+++ profiles/generic/desktop.nix
@@ -6,18 +6,8 @@
 { pkgs, args, ... }:
 {
   imports = [
-    ../../modules/terminal
-    ../../modules/hardware/audio.nix
-    ../../modules/hardware/bluetooth.nix
-    ../../modules/hardware/firmware.nix
-    ../../modules/hardware/printers.nix
-    ../../modules/hardware/video.nix
-    ../../modules/desktop/backgrounds/opt.nix
-    ../../modules/desktop/icons.nix
+    ./light.nix
   ];
-
-  # Enable XFCE and all needed components
-  services.xserver.desktopManager.xfce.enable = true;
 
   # Gaming
   programs.steam.enable = true;         # Digital distribution platform from Valve
@@ -26,56 +16,15 @@
   programs.winetricks.enable = true;    # A script to install DLLs needed to work around problems in Wine
   programs.warcraft2.enable = true;     # Add firewall rules needed for warcraft 2 IPX LAN multi-player
 
-  # Office
-  programs.geany.enable = true;         # Simple text editor
-  programs.evince.enable = true;        # Document viewer for PDF, djvu, tiff, dvi, XPS, cbr, cbz, cb7, cbt
-
   # Multimedia
   programs.kodi = {                     # Media player and entertainment hub
     enable = true;
     remoteControlHTTP = true;
   };
-  programs.qview.enable = true;         # Simple image viewer with webp support
-  programs.smplayer.enable = true;      # UI wrapper around mplayer with click to pause
-
-  # Network
-  programs.firefox.enable = true;       # Mozilla browser
-  programs.filezilla.enable = true;     # Network/Transfer
-  programs.qbittorrent.enable = true;   # Excellent bittorrent client
-
-  # System
-  programs.dmenu.enable = true;         # Configure dmenu
-  programs.file-roller.enable = true;   # Generic Gnome file archive utility needed for Thunar
-  programs.hardinfo.enable = true;      # A system information and benchmark tool
-  services.fwupd.enable = true;         # Firmware update tool for BIOS, etc...
-  services.gvfs.enable = true;          # GVfs virtual filesystem
-
-  # Optionally enable client nfs shares
-  services.nfs.client.shares.enable = args.settings.nfs_shares;
-
-  # Configure gnome keyring for VPN and Copilot and automatically unlock on login
-  services.gnome.gnome-keyring.enable = true;
-  security.pam.services.lightdm.enableGnomeKeyring = true;
 
   environment.systemPackages = with pkgs; [
 
-    # System
-    desktop-file-utils                  # Command line utilities for working with desktop entries
-    filelight                           # View disk usage information
-#    gnome-dconf-editor                 # General configuration manager that replaces gconf
-    i3lock-color                        # Simple lightweight screen locker
-    paprefs                             # Pulse audio server preferences for simultaneous output
-
-    # VPN
-    networkmanager-openvpn              # NetworkManager VPN plugin for OpenVPN
-    openvpn                             # An easy-to-use, robust and highly configurable VPN (Virtual Private Network)
-
     # Network
-    freerdp                             # RDP client plugin for remmina
-    remmina                             # Nice remoting UI for RDP and other protocols
-    #tdesktop                            # Telegram Desktop messaging app
-    vopono                              # Run applications through VPN connections in network namespaces
-    update-systemd-resolved             # OpenVPN systemd-resolved updater
     zoom-us                             # Video conferencing application
 
     # Media
@@ -112,23 +61,10 @@
     # Office
     hunspell                            # LibreOffice spell checker and actively maintained
     hunspellDicts.en_US                 # LibreOffice spell checker and actively maintained
-    keepassxc                           # Offline password manager with many features
     libreoffice-fresh                   # Comprehensive, professional-quality productivity suite
     scribus                             # Open Source Desktop Publishing
 
     # Utilities
-    alacritty                           # GPU accelerated terminal
-    alacritty-theme                     # GPU accelerated terminal themes
     awf                                 # A widget factory for viewing theme changes
-    conky                               # Advanced, highly configurable system monitor
-    exiftool                            # A tool to read, write and edit EXIF meta information
-    galculator                          # Simple calculator
-    gnome-multi-writer                  # Tool for writing an ISO file to multiple USB devices at once
-    htop                                # Better top tool
-    light                               # Control backlights for screen and keyboard
-    veracrypt                           # Free Open-Source filesystem encryption
-
-    # System
-    jdk17                               # Needed for: minecraft
   ];
 }
