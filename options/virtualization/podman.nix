91eee7f7698dd4a2a87f4f581d621816e4e4e525
phR0ze <prc451@gmail.com>
2024-12-04 17:03:12 -0700

    Improving podman configuration

--- options/virtualization/podman.nix
+++ options/virtualization/podman.nix
@@ -1,5 +1,16 @@
 # Podman configuration
 #
+# ### Prerequisites
+# The following kernel params already setup in nixos-config/modules/boot/kerel.nix are required
+# ```nix
+# boot.kernel.sysctl = {
+#   "net.ipv4.ip_forward" = 1;
+#   "net.bridge.bridge-nf-call-arptables" = 0;
+#   "net.bridge.bridge-nf-call-ip6tables" = 0;
+#   "net.bridge.bridge-nf-call-iptables" = 0;
+# };
+# ```
+#
 # ### Features
 # - purposefully renaming `virtualization` to give me a new namespace to work in
 #
@@ -21,12 +32,23 @@ in
     # Allow primary user access to podman
     users.users.${args.settings.username}.extraGroups = [ "podman" ];
 
+    # Install dependencies
+    environment.systemPackages = [
+      pkgs.podman-compose
+    ];
+
+    # Set the default backend container technology to podman
+    virtualisation.oci-containers.backend = "podman";
+
+    # Enable container name DNS for non-default Podman networks.
+    # https://github.com/NixOS/nixpkgs/issues/226365
+    networking.firewall.interfaces."podman+".allowedUDPPorts = [ 53 ];
+
+    # Enable and configure podman
     virtualisation.podman = {
       enable = true;
       dockerCompat = true; # provide docker alias
-      extraPackages = [
-        pkgs.podman-compose
-      ];
+      #dockerSocket.enable = true;
 
       # Allows docker containers to refer to each other by name
       defaultNetwork.settings.dns_enabled = true;
