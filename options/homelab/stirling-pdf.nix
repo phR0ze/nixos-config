559380c19105578904882453d344096a16b670d5
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-12-05 12:35:28 -0700

    Implemented dedicated macvlan with static ip pattern

--- options/homelab/stirling-pdf.nix
+++ options/homelab/stirling-pdf.nix
@@ -8,14 +8,19 @@
 # reorganizing, adding images, rotating, compressing, and more. This locally hosted web application 
 # has evolved to encompass a comprehensive set of features, addressing all your PDF requirements.
 #
-# - Web Interface
 # - Stirling-PDF does not initiate any outbound calls for record-keeping or tracking purposes.
 # - All files and PDFs exist either exclusively on the client side, server memory only during task 
 #   execution, or as temporary files solely for the execution of the task.
+#
+# ### Deployment Features
+# - App has outbound access to the internet
+# - App is blocked from outbound connections to the LAN
+# - App is visiable on the LAN, with a dedicated host macvlan and static IP, for inbound connections
+# - App data is persisted at /var/lib/$APP
 # --------------------------------------------------------------------------------------------------
 { config, lib, pkgs, args, ... }: with lib.types;
 let
-  cfg = config.homelab.stirling-pdf;
+  app = config.homelab.stirling-pdf;
 in
 {
   options = {
@@ -23,19 +28,25 @@ in
       enable = lib.mkEnableOption "Deploy container based Stirling PDF";
 
       name = lib.mkOption {
-        description = lib.mdDoc "App name to use for supporting components.";
+        description = lib.mdDoc "App name to use for supporting components";
         type = types.str;
         default = "stirling-pdf";
       };
 
+      nic = lib.mkOption {
+        description = lib.mdDoc "Parent NIC for the app macvlan";
+        type = types.str;
+        default = "${args.settings.nic0}";
+      };
+
       ip = lib.mkOption {
-        description = lib.mdDoc "IP address to use for the app macvlan.";
+        description = lib.mdDoc "IP address to use for the app macvlan";
         type = types.str;
         default = "192.168.1.60";
       };
 
       port = lib.mkOption {
-        description = lib.mdDoc "Port to use for Web Interface.";
+        description = lib.mdDoc "Port to use for Web Interface on the macvlan";
         type = types.port;
         default = 80;
         example = {
@@ -45,31 +56,50 @@ in
     };
   };
  
-  config = lib.mkIf cfg.enable {
+  config = lib.mkIf app.enable {
+    assertions = [
+      {
+        assertion = ("${app.nic}" != "");
+        message = "Application parent NIC not specified, please set 'nic'";
+      }
+    ];
 
-    # Generate the "podman-${cfg.name}" service unit for the container
+    # Create persistent directories for application
+    systemd.tmpfiles.rules = [
+      # type, path, mode, user, group, expiration
+      # No group specified, i.e `-` defaults to root
+      # No age specified, i.e `-` defaults to infinite
+      "d /var/lib/${app.name} 0750 ${args.settings.username} - -"
+      "d /var/lib/${app.name}/customFiles 0750 ${args.settings.username} - -"
+      "d /var/lib/${app.name}/extraConfigs 0750 ${args.settings.username} - -"
+      "d /var/lib/${app.name}/logs 0750 ${args.settings.username} - -"
+      "d /var/lib/${app.name}/pipeline 0750 ${args.settings.username} - -"
+      "d /var/lib/${app.name}/trainingData 0750 ${args.settings.username} - -"
+    ];
+
+    # Generate the "podman-${app.name}" service unit for the container
     # https://docs.stirlingpdf.com/Getting%20started/Installation/Docker/Docker%20Install
-    virtualisation.oci-containers.containers."${cfg.name}" = {
+    virtualisation.oci-containers.containers."${app.name}" = {
       image = "docker.io/frooodle/s-pdf:latest";
       autoStart = true;
       ports = [
-        "${cfg.ip}:${toString cfg.port}:8080"
+        "${app.ip}:${toString app.port}:8080"
       ];
       volumes = [
-        "/var/lib/${cfg.name}/customFiles:/customFiles:rw"
-        "/var/lib/${cfg.name}/extraConfigs:/configs:rw"
-        "/var/lib/${cfg.name}/logs:/logs:rw"
-        "/var/lib/${cfg.name}/pipeline:/pipeline:rw"
-        "/var/lib/${cfg.name}/trainingData:/usr/share/tessdata:rw"
+        "/var/lib/${app.name}/customFiles:/customFiles:rw"
+        "/var/lib/${app.name}/extraConfigs:/configs:rw"
+        "/var/lib/${app.name}/logs:/logs:rw"
+        "/var/lib/${app.name}/pipeline:/pipeline:rw"
+        "/var/lib/${app.name}/trainingData:/usr/share/tessdata:rw"
       ];
-#      environment = {
-#        "DOCKER_ENABLE_SECURITY" = "false";
-#        "INSTALL_BOOK_AND_ADVANCED_HTML_OPS" = "false";
-#        "LANGS" = "en_US";
-#      };
-#      environmentFiles = [
-#        default.env
-#      ];
+
+      # Configure app via overrides
+      environment = {
+        "METRICS_ENABLED" = "false";                    # no need to track with homelab
+        "SYSTEM_ENABLEANALYTICS" = "false";             # not a fan of being tracked
+        "DOCKER_ENABLE_SECURITY" = "false";             # don't need to login with homelab
+        "INSTALL_BOOK_AND_ADVANCED_HTML_OPS" = "false";
+      };
 #      extraOptions = [
 #        "--network-alias=stirling-pdf"
 #        "--network=stirling-pdf_default"
@@ -82,34 +112,38 @@ in
 #      };
     };
 
-    # Open up firewall on host macvlan for new app service
-    networking.firewall.interfaces.${cfg.name}.allowedTCPPorts = [ 80 ];
+    # Create host macvlan with a dedicated static IP for the application to port forward to
+    networking = {
+      macvlans.${app.name} = {
+        interface = "${app.nic}";
+        mode = "bridge";
+      };
+      interfaces.${app.name}.ipv4.addresses = [
+        { address = "${app.ip}"; prefixLength = 32; }
+      ];
+      firewall.interfaces.${app.name}.allowedTCPPorts = [ 80 ];
+    };
 
-    # Create persistent directories for application
-    systemd.tmpfiles.rules = [
-      # type, path, mode, user, group, expiration
-      # No group specified, i.e `-` defaults to root
-      "d /var/lib/${cfg.name} 0750 ${args.settings.username} - -"
-      "d /var/lib/${cfg.name}/customFiles 0750 ${args.settings.username} - -"
-      "d /var/lib/${cfg.name}/extraConfigs 0750 ${args.settings.username} - -"
-      "d /var/lib/${cfg.name}/logs 0750 ${args.settings.username} - -"
-      "d /var/lib/${cfg.name}/pipeline 0750 ${args.settings.username} - -"
-      "d /var/lib/${cfg.name}/trainingData 0750 ${args.settings.username} - -"
-    ];
+    # Add additional configuration to the above generated app service unit i.e. acts as an overlay.
+    # We simply match the name here that is autogenerated from the oci-container directive.
+    systemd.services."podman-${app.name}" = {
+      wantedBy = [ "multi-user.target" ];
 
-    # Add additional configuration to the above generated app service unit
-    systemd.services."podman-${cfg.name}" = {
-      #wantedBy = [ "multi-user.target" ];
-      #wants = [ "network-online.target" ];
-      #after = [ "network-online.target" ];
-#      environment = {
-#        "XDG_DATA_HOME" = "/var/lib/${cfg.name}/data";
-#        "XDG_CACHE_HOME" = "/var/lib/${cfg.name}/cache";
-#        "XDG_CONFIG_HOME" = "/var/lib/${cfg.name}/config";
-#      };
+      # Trigger the creation of the app macvlan if not already and wait for it. network-addresses... 
+      # applies the static IP address to the macvlan which it waits to be created for, thus by 
+      # waiting on it we ensure the macvlan is up and running with an IP address.
+      wants = [
+        "network-online.target"
+        "network-addresses-${app.name}.service"
+      ];
+      after = [
+        "network-online.target"
+        "network-addresses-${app.name}.service"
+      ];
+
       serviceConfig = {
         Restart = "always";
-        WorkingDirectory = "/var/lib/${cfg.name}";
+        WorkingDirectory = "/var/lib/${app.name}";
       };
     };
   };
