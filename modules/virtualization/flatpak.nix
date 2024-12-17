f20ca1270cd0e2c700d863d0bad5bcac7609beac
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-12-17 16:14:51 -0700

    Dropping defunct modules

--- modules/virtualization/flatpak.nix
+++
@@ -1,8 +0,0 @@
-# Podman configuration
-#
-# ### Details
-# - https://nixos.org/manual/nixos/unstable/#module-services-flatpak
-#---------------------------------------------------------------------------------------------------
-{ args, ... }:
-{
-}
