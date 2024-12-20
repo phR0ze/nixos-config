a6d3c75550ff24d78dd3f5ba1550382d52f7f688
phR0ze <prc451@gmail.com>
2024-12-20 00:09:18 -0700

    Attempting to read in yaml args

--- machines/homelab/default.nix
+++ machines/homelab/default.nix
@@ -4,10 +4,18 @@
 # - ?
 # --------------------------------------------------------------------------------------------------
 { pkgs, lib, f, ... }:
+let
+  local_args = f.fromYAML ./args.dec.yaml;
+in
 {
   imports = [
     ../../options
     ../../profiles/server.nix
     ./hardware-configuration.nix
+  ];
+
+  assertions = [
+    { assertion = (builtins.length local_args.username == "");
+      message = "username: ${local_args.username}"; }
   ];
 }
