072d567962b2f88936c8b9ef46bc1b0481506a97
phR0ze <28798432+phR0ze@users.noreply.github.com>
2024-12-27 12:21:06 -0700

    Fix flake pathing issue

--- machines/macbook/args.nix
+++ machines/macbook/args.nix
@@ -1,9 +1,5 @@
 {
   hostname = "macbook";             # hostname for the machine
-  nfs = false;                      # enable the nfs client shares for this system
-  autologin = false;                # automatically log the user in after boot when true
-
   efi = true;                       # EFI system boot type, default "false"
-  mbr = "nodev";                    # MBR system boot device, e.g. /dev/sda, default "nodev"
-  nic0 = "ens18";                   # First NIC found in hardware-configuration.nix
+  nic0 = "enp125s0u1u2u1";          # First NIC found in hardware-configuration.nix
 }
