9a2e18ad72cc88b0588ce8edf94795e2570b137d
phR0ze <prc451@gmail.com>
2025-01-09 17:57:15 -0700

    Fixes for macbook

--- machines/macbook/args.nix
+++ machines/macbook/args.nix
@@ -1,5 +1,5 @@
 {
   hostname = "macbook";             # hostname for the machine
   efi = true;                       # EFI system boot type, default "false"
-  nic0 = "enp125s0u1u2u1";          # First NIC found in hardware-configuration.nix
+  nic0_name = "enp125s0u1u2u1";     # First NIC found in hardware-configuration.nix
 }
