22af19c8dc2e222f421e4d940066f2409a16645b
phR0ze <prc451@gmail.com>
2025-01-11 14:38:43 -0700

    Migrating homelab config to machine paradigm

--- machines/homelab/args.nix
+++ machines/homelab/args.nix
@@ -1,18 +1,13 @@
 {
-  hostname = "homelab";             # hostname for the machine
-  nfs = false;                      # enable the nfs client shares for this system
-  autologin = false;                # automatically log the user in after boot when true
+  hostname = "homelab";
+  shares_enable = false;
+  efi = false;
+  mbr = "/dev/sda";
+  autologin = false;
+  nic0_name = "ens18";
 
-  efi = false;                      # EFI system boot type, default "false"
-  mbr = "/dev/sda";                 # MBR system boot device, e.g. /dev/sda, default "nodev"
-  nic0 = "ens18";                   # First NIC found in hardware-configuration.nix
-  nic1 = "";                        # Second NIC found in hardware-configuration.nix
-
-  # Virtual machines systemd units
-  # - prior to `clu update system` build the vms with `clu build vm --help`
-  # -----------------------------------------------------------------------------------------------
-  vms = [
-    { enable = false; hostname = "nixos70"; }
-    { enable = false; hostname = "nixos71"; }
-  ];
+#  vms = [
+#    { enable = false; hostname = "nixos70"; }
+#    { enable = false; hostname = "nixos71"; }
+#  ];
 }
