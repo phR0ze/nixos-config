b4b3639ef680eaac638268da4c6669a9609b1359
phR0ze <prc451@gmail.com>
2025-01-11 16:17:30 -0700

    Working on Nix binary cache support

---
+++ options/services/cache.nix
@@ -0,0 +1,44 @@
+# Nix Binary Cache configuration
+#
+# --------------------------------------------------------------------------------------------------
+{ config, lib, pkgs, ... }: with lib.types;
+let
+  cfg = config.services.cache;
+  virtualhost = "cache";
+  decrypted_key_path = "/var/lib/cache/private.dec.pem";
+
+  decrypted_key_pkg = pkgs.runCommandLocal "decrypted_key" {} ''
+    mkdir $out
+    decrypted="$out${decrypted_key_path}"
+    ${pkgs.sops}/bin/sops decrypt --output "$decrypted" "${../../include/var/lib/cache/private.enc.pem}" 
+  '';
+in
+{
+  options = {
+    services.cache = {
+      enable = lib.mkEnableOption "Install and configure Nix Binary Cache";
+    };
+  };
+ 
+  config = lib.mkIf (cfg.enable) {
+
+    # Configure nix-serve to serve up the nix store as a binary cache with package signing
+    services.nix-serve = {
+      enable = true;
+      secretKeyFile = decrypted_key_path;
+    };
+
+    services.nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      virtualHosts."${virtualhost}" = {
+        locations."/".proxyPass = "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
+      };
+    };
+ 
+    # Open up the firewall for port 80
+    networking.firewall.allowedTCPPorts = [
+      config.services.nginx.defaultHTTPListenPort
+    ];
+  };
+}
