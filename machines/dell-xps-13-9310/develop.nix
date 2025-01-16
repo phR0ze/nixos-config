686318454c8b66eded4ebfdf5f5ddc7fa0fe6f9b
phR0ze <28798432+phR0ze@users.noreply.github.com>
2025-01-16 15:49:10 -0700

    Re-enable dhcp as fallback

--- machines/dell-xps-13-9310/develop.nix
+++ machines/dell-xps-13-9310/develop.nix
@@ -11,6 +11,6 @@
 
   hardware.graphics.intel = true;
   services.xserver.xft.dpi = 115;
+  virtualization.host.enable = true;
   services.x11vnc.enable = lib.mkForce false;
-  virtualization.virt-manager.enable = true;
 }
--- options/network/default.nix
+++ options/network/default.nix
@@ -94,42 +94,30 @@ in
 
     # Configure IP address for primary interface
     # ----------------------------------------------------------------------------------------------
-    {
-      networking.interfaces."${nic0.name}".ipv4.addresses = [ nic0.ip.attrs ];
-      networking.defaultGateway = "${nic0.gateway}";
-    }
 
     # Optionally configure network bridge with static IP
-#    (f.mkIfElse (cfg.bridge.enable && nic0.ip.full != "") {
-#      assertions = [
-#        { assertion = (nic0.name != "");
-#          message = "NIC0 was not specified, please set 'nic0.name'"; }
-#      ];
-#
-#      networking.useDHCP = false;
-#      networking.bridges."${cfg.bridge.name}".interfaces = [ "${nic0.name}" ];
-#      networking.interfaces."${cfg.bridge.name}".ipv4.addresses = [ nic0.ip.attrs ];
-#      networking.defaultGateway = "${nic0.gateway}";
-#
-#    # Otherwise configure network bridge with DHCP second
-#    } (f.mkIfElse (cfg.bridge.enable && nic0.ip.full == "") {
-#      assertions = [
-#        { assertion = (nic0.name != "");
-#          message = "NIC0 was not specified, please set 'nic0.name'"; }
-#      ];
-#
-#      networking.useDHCP = false;
-#      networking.bridges."${cfg.bridge.name}".interfaces = [ "${nic0.name}" ];
-#      networking.interfaces."${cfg.bridge.name}".useDHCP = true;
-#
-#    # Otherwise configure static IP for the primary NIC third
-#    } (f.mkIfElse (nic0.ip.full != "") {
-#      networking.interfaces."${nic0.name}".ipv4.addresses = [ nic0.ip.attrs ];
-#      networking.defaultGateway = "${nic0.gateway}";
-#
-#    # Finally fallback on DHCP for the primary NIC
-#    } {
-#      # configure DHCP
-#    })))
+    (f.mkIfElse (cfg.bridge.enable && nic0.ip.full != "") {
+      assertions = [ { assertion = (nic0.name != ""); message = "NIC0 was not specified, please set 'nic0.name'"; } ];
+      networking.useDHCP = false;
+      networking.bridges."${cfg.bridge.name}".interfaces = [ "${nic0.name}" ];
+      networking.interfaces."${cfg.bridge.name}".ipv4.addresses = [ nic0.ip.attrs ];
+      networking.defaultGateway = "${nic0.gateway}";
+
+    # Otherwise configure network bridge with DHCP second
+    } (f.mkIfElse (cfg.bridge.enable && nic0.ip.full == "") {
+      assertions = [ { assertion = (nic0.name != ""); message = "NIC0 was not specified, please set 'nic0.name'"; } ];
+      networking.useDHCP = false;
+      networking.bridges."${cfg.bridge.name}".interfaces = [ "${nic0.name}" ];
+      networking.interfaces."${cfg.bridge.name}".useDHCP = true;
+
+    # Otherwise configure static IP for the primary NIC third
+    } (f.mkIfElse (nic0.ip.full != "") {
+      networking.interfaces."${nic0.name}".ipv4.addresses = [ nic0.ip.attrs ];
+      networking.defaultGateway = "${nic0.gateway}";
+
+    # Finally fallback on DHCP for the primary NIC
+    } {
+      # No configuartion here will default to DHCP
+    })))
   ];
 }
