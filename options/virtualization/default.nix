8990a2ffb06c6aa30252ae3154b8c9fecb07b908
phR0ze <28798432+phR0ze@users.noreply.github.com>
2025-01-17 12:32:27 -0700

    Splitting qemu options into host and guest facing

--- options/virtualization/default.nix
+++ options/virtualization/default.nix
@@ -1,11 +1,9 @@
-# Import all the options
-#---------------------------------------------------------------------------------------------------
-{ config, lib, pkgs, f, ... }:
+{ ... }:
 {
   imports = [
     ./incus.nix
     ./podman.nix
-    ./qemu.nix
+    ./qemu
     ./virt-manager.nix
     ./winetricks.nix
   ];
