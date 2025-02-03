e9043b0cf30d59a6d6994d48fcfdf02f8f276533
phR0ze <28798432+phR0ze@users.noreply.github.com>
2025-02-03 16:29:14 -0700

    Fix for machine vm type

--- profiles/base.nix
+++ profiles/base.nix
@@ -52,7 +52,7 @@ in
     vopono                        # Run applications through VPN connections in network namespaces
     update-systemd-resolved       # OpenVPN systemd-resolved updater
   ]
-  ++ lib.optional (machine.vm == {}) efibootmgr
-  ++ lib.optional (machine.vm == {}) efivar
+  ++ lib.optional (!machine.vm.any) efibootmgr
+  ++ lib.optional (!machine.vm.any) efivar
   ;
 }
