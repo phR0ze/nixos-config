1b169b25ff1153a98e61e4dfa46b8842fdd453fd
phR0ze <prc451@gmail.com>
2025-02-06 10:41:40 -0700

    Working on a non-destructive build option

--- machines/workstation/hardware-configuration.nix
+++ machines/workstation/hardware-configuration.nix
@@ -7,7 +7,10 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "ata_generic" "ehci_pci" "ahci" "isci" "xhci_pci" "firewire_ohci" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [
+    "ata_generic" "ehci_pci" "ahci" "isci" "xhci_pci"
+    "firewire_ohci" "usb_storage" "usbhid" "sd_mod" "sr_mod"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
