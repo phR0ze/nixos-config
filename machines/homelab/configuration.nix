971e09052692e11c1ea94bedbebdc866bd740091
phR0ze <28798432+phR0ze@users.noreply.github.com>
2025-02-05 19:44:15 -0700

    Adding recursive override for homelab

--- machines/homelab/configuration.nix
+++ machines/homelab/configuration.nix
@@ -3,7 +3,7 @@
 { config, pkgs, lib, args, f, ... }: with lib.types;
 let
   cfg = config.machine;
-  _args = args // (import ./args.nix) // (f.fromJSON ./args.dec.json);
+  _args = lib.recursiveUpdate args (lib.recursiveUpdate (import ./args.nix) (f.fromJSON ./args.dec.json));
 in
 {
   imports = [
