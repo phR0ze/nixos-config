62ad57135154b8947eecdb16908fef2e68f85aa3
phR0ze <prc451@gmail.com>
2025-02-15 14:33:21 -0700

    Accounting for new prismlauncher 9.1 version

--- options/apps/games/prismlauncher.nix
+++ options/apps/games/prismlauncher.nix
@@ -1,54 +1,63 @@
 # Prismlauncher options
+#
+# ### Patches
+# - provide offline support
 #---------------------------------------------------------------------------------------------------
 { config, lib, pkgs, ... }: with lib.types;
 let
   cfg = config.apps.games.prismlauncher;
-
-  cfgfile = lib.mkIf cfg.enable (pkgs.writeText "prismlauncher.cfg" ''
-    [General]
-    ApplicationTheme=system
-    ConfigVersion=1.2
-    IconTheme=pe_colored
-    JavaPath=${cfg.javaPath}
-    Language=en_US
-    LastHostname=${config.machine.hostname}
-    MaxMemAlloc=${toString cfg.maxMemAlloc}
-    MinMemAlloc=${toString cfg.minMemAlloc}
-  '');
 in
 {
   options = {
     apps.games.prismlauncher = {
       enable = lib.mkEnableOption "Install and configure PrismLauncher";
-
       maxMemAlloc = lib.mkOption {
+        description = lib.mdDoc "Max memory to allocate for minecraft client";
         type = types.int;
         default = 4096;
-        description = lib.mdDoc "Max memory to allocate for minecraft client";
       };
       minMemAlloc = lib.mkOption {
+        description = lib.mdDoc "Min memory to allocate for minecraft client";
         type = types.int;
         default = 512;
-        description = lib.mdDoc "Min memory to allocate for minecraft client";
       };
       javaPath = lib.mkOption {
+        description = lib.mdDoc "Path to use to find java";
         type = types.str;
         default = "java";
-        description = lib.mdDoc "Path to use to find java";
+      };
+      tag = lib.mkOption {
+        description = lib.mdDoc "Version tag to match patches against";
+        type = types.str;
+        default = "v9.1";
       };
     };
   };
 
   config = lib.mkIf (cfg.enable) {
 
+    # Patch the app then install it
     environment.systemPackages = with pkgs; [
       (prismlauncher.override (prev: {
         prismlauncher-unwrapped = prev.prismlauncher-unwrapped.overrideAttrs (o: {
-          patches = (o.patches or [ ]) ++ [ ../../../patches/prismlauncher/offline.patch ];
+          patches = (o.patches or [ ]) ++ [ ../../../patches/prismlauncher/${cfg.tag}/offline.patch ];
         });
       }))
     ];
 
-    files.user.".local/share/PrismLauncher/prismlauncher.cfg".copy = cfgfile;
+    # Create the initial user configuration
+    files.user.".local/share/PrismLauncher/prismlauncher.cfg".copy = (
+      pkgs.writeText "prismlauncher.cfg" ''
+        [General]
+        ApplicationTheme=system
+        ConfigVersion=1.2
+        IconTheme=pe_colored
+        JavaPath=${cfg.javaPath}
+        Language=en_US
+        LastHostname=${config.machine.hostname}
+        MaxMemAlloc=${toString cfg.maxMemAlloc}
+        MinMemAlloc=${toString cfg.minMemAlloc}
+      ''
+    );
   };
 }
