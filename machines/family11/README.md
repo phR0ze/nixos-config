7c2ba2137f89817ee961aea5eee8bdb89681d075
phR0ze <28798432+phR0ze@users.noreply.github.com>
2025-02-19 12:59:41 -0700

    Adding family 11 config

---
+++ machines/family11/README.md
@@ -0,0 +1,18 @@
+# HP Z220 SFF <img style="margin: 6px 13px 0px 0px" align="left" src="../../art/logo_36x36.png" />
+
+### Quick links
+* [.. up dir](../../README.md)
+* [Device specs](#device-specs)
+
+## Device specs
+* Core
+  * 4-Core Intel Xeon E3-1280 @ 1.60-3.60 GHz
+  * 8 GB RAM
+* Graphics
+  * Nvidia GK197GL [Quadro K600] nouveau
+* Drives
+  * 250 GB Micron SSD
+
+<!-- 
+vim: ts=2:sw=2:sts=2
+-->
---
+++ machines/family11/configuration.nix
@@ -0,0 +1,29 @@
+# Family11 configuration
+#
+# ### Features
+# - Basic desktop deployment
+# --------------------------------------------------------------------------------------------------
+{ config, pkgs, lib, args, f, ... }: with lib.types;
+let
+  cfg = config.machine;
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../options/types/validate_machine.nix
+    (../../profiles/${args.profile}.nix)
+  ];
+
+  options = {
+    machine = lib.mkOption {
+      description = lib.mdDoc "Machine arguments";
+      type = types.submodule (import ../../options/types/machine.nix { inherit lib args f; });
+    };
+  };
+
+  config = {
+    machine.type.bootable = true;
+    machine.nix.cache.enable = true;
+    apps.games.warcraft2.enable = true;
+  };
+}
