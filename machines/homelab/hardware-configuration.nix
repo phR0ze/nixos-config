c47238ee120bf8ce5f52fbfd4097a68b83b2d344
phR0ze <28798432+phR0ze@users.noreply.github.com>
2025-02-27 19:34:33 -0700

    Adding storage drive to homelab

--- machines/homelab/hardware-configuration.nix
+++ machines/homelab/hardware-configuration.nix
@@ -29,6 +29,11 @@
     { device = "/dev/disk/by-uuid/819b5842-b516-4772-b83d-96916f1822af"; }
   ];
 
+  fileSystems."/mnt/storage1" = {
+    device = "/dev/disk/by-uuid/${(builtins.elemAt config.machine.drives 1).uuid}";
+    fsType = "ext4";
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
