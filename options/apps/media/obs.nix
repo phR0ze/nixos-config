f2b8e4e6a401decda01a2b5be568341f10be53b7
phR0ze <prc451@gmail.com>
2025-03-06 17:57:35 -0700

    Disable firewall for NDI temporarily

--- options/apps/media/obs.nix
+++ options/apps/media/obs.nix
@@ -6,6 +6,8 @@
 # ### Plugins
 # - [Background removal plugin](https://github.com/locaal-ai/obs-backgroundremoval)
 #
+# ### TODO:
+# * Determine the correct ports to open
 #---------------------------------------------------------------------------------------------------
 { config, lib, pkgs, ... }: with lib.types;
 let
@@ -23,17 +25,30 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [
-      (pkgs.wrapOBS {
-        plugins = with pkgs.obs-studio-plugins; [
-          obs-backgroundremoval                 # Remove or blur the video background
-          obs-pipewire-audio-capture            # Capture appliation audio with PipeWire
-        ] ++ lib.optionals cfg.ndi [ obs-ndi ]; # Optional NDI support
-      })
-    ];
+  config = lib.mkMerge [
 
-    # polkit needs to be enabled so that OBS can access the virtual camera device
-    security.polkit.enable = true;
+    # Enable OBS Studio
+    (lib.mkIf cfg.enable {
+      environment.systemPackages = [
+        (pkgs.wrapOBS {
+          plugins = with pkgs.obs-studio-plugins; [
+            obs-backgroundremoval                 # Remove or blur the video background
+            obs-pipewire-audio-capture            # Capture appliation audio with PipeWire
+          ] ++ lib.optionals cfg.ndi [ obs-ndi ]; # Optional NDI support
+        })
+      ];
+
+      # polkit needs to be enabled so that OBS can access the virtual camera device
+      security.polkit.enable = true;
+    })
+
+    # Optionally open firewall ports for NDI
+    (lib.mkIf cfg.ndi {
+      networking.firewall.enable = false;
+      #networking.firewall.allowedTCPPorts = [
+      #  5959                                      # NDI Discovery server port
+      #  5960                                      # NDI remote soruces to discover this machine
+      #];
+    })
   };
 }
