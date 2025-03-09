65c8df065c76eef786e3cd5574c1696c5387ee1e
phR0ze <prc451@gmail.com>
2025-03-08 19:50:20 -0700

    Working on xps13 conversion

---
+++ machines/xps13/configuration.nix
@@ -0,0 +1,39 @@
+# XPS13 configuration
+#
+# ### Features
+# - Daily driver desktop deployment
+# --------------------------------------------------------------------------------------------------
+{ config, pkgs, lib, args, f, ... }: with lib.types;
+let
+  cfg = config.machine;
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../options/types/validate_machine.nix
+    (../../profiles/${args.profile}.nix)
+  ];
+
+  options = {
+    machine = lib.mkOption {
+      description = lib.mdDoc "Machine arguments";
+      type = types.submodule (import ../../options/types/machine.nix { inherit lib args f; });
+    };
+  };
+
+  config = {
+    machine.type.bootable = true;
+    hardware.graphics.intel = true;
+    services.xserver.xft.dpi = 115;
+
+    #machine.net.bridge.enable = true;
+    virtualisation.podman.enable = true;
+    #virtualisation.qemu.host.enable = true;
+    services.raw.rustdesk.autostart = false;
+
+    # Misc
+    environment.systemPackages = [
+      pkgs.freetube
+    ];
+  };
+}
--- machines/xps13/hardware-configuration.nix
+++ machines/xps13/hardware-configuration.nix
@@ -5,8 +5,7 @@
   ];
 
   boot.initrd.availableKernelModules = [
-    "ata_generic" "ehci_pci" "ahci" "isci" "xhci_pci"
-    "firewire_ohci" "usb_storage" "usbhid" "sd_mod" "sr_mod"
+    "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"
   ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
@@ -18,31 +17,21 @@
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/EED1-1E99";
+    device = "/dev/disk/by-uuid/68E4-CF3D";
     fsType = "vfat";
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-uuid/64b00b8b-00a0-4b92-b273-46559044ad50"; }
+    { device = "/dev/disk/by-uuid/b135c957-27a6-4369-a2c8-472a912e12ac"; }
   ];
-
-  fileSystems."/mnt/storage1" = {
-    device = "/dev/disk/by-uuid/${(builtins.elemAt config.machine.drives 1).uuid}";
-    fsType = "ext4";
-  };
-
-  fileSystems."/mnt/storage2" = {
-    device = "/dev/disk/by-uuid/${(builtins.elemAt config.machine.drives 2).uuid}";
-    fsType = "ext4";
-  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s13f0u3u2u1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
