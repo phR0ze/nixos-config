71be71fb88efd2035f8befb226fdcf8e2f9a4116
phR0ze <28798432+phR0ze@users.noreply.github.com>
2025-03-14 17:11:52 -0600

    Working on Flutter support

--- options/development/rust.nix
+++ options/development/rust.nix
@@ -34,5 +34,24 @@ in
       openssl.dev                         # Development headers for Open SSL
       gtk4                                # Multi-platform toolkit for creating graphical user interfaces
     ];
+
+#    rustToolchain =
+#            # This should be kept in sync with the value in bazel/rust/defs.bzl
+#            pkgs.rust-bin.nightly."2024-09-05".default.override {
+#              extensions = [
+#                "clippy"
+#                "llvm-tools-preview"
+#                "rust-analyzer"
+#                "rust-src"
+#                "rustfmt"
+#              ];
+#              targets = [
+#                "wasm32-unknown-unknown"
+#                "x86_64-unknown-linux-musl"
+#                "x86_64-unknown-none"
+#              ];
+#            };
+#          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+#          src = ./.;
   };
 }
