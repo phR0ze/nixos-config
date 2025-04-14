e6547062d644091d9276dee28b2fe4d349c6dca8
phR0ze <28798432+phR0ze@users.noreply.github.com>
2025-04-14 09:25:51 -0600

    Adding chm support

--- machines/workstation/configuration.nix
+++ machines/workstation/configuration.nix
@@ -40,6 +40,7 @@ in
 
     # Misc
     environment.systemPackages = [
+      pkgs.xchm                     # App for reading Microsoft help files for technical manuals
       pkgs.synology-drive-client
       pkgs.freetube
       pkgs.wiiload
