7fd1eedc33eb5a751826cb88a2753c072024f667
phR0ze <prc451@gmail.com>
2025-08-11 17:43:39 -0600

    Rolling to a new pseudo stable

--- base.nix
+++ base.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/3566ab7246670a43abd2ffa913cc62dad9cdf7d5";
+    nixpkgs.url = "github:nixos/nixpkgs/5b09dc45f24cf32316283e62aec81ffee3c3e376";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
