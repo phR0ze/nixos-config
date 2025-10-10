ee4c583d7c970a659d619f1d114fcbad7e0e1001
phR0ze <28798432+phR0ze@users.noreply.github.com>
2025-10-09 22:48:17 -0600

    Working custom plugin auto load

--- options/apps/system/nvim/init-plugin/init.lua
+++
@@ -1,4 +0,0 @@
-require("config.options")               -- load options from ./lua/config/options.lua
-require("config.keymaps")               -- load keymaps from ./lua/config/keymaps.lua
-
---require("lz.n").load("plugins")         -- load plugins from ./lua/plugins
