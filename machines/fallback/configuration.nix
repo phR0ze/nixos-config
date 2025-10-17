2755f8cf8a96083decb3c0235dc850ba503175d4
phR0ze <prc451@gmail.com>
2025-10-17 17:01:39 -0600

    Switch over to Nvidia proprietary drivers for fallback system

--- machines/fallback/configuration.nix
+++ machines/fallback/configuration.nix
@@ -19,6 +19,7 @@
     machine.type.bootable = true;
     machine.nix.cache.enable = true;
     apps.games.warcraft2.enable = true;
+    hardware.gpu.nvidiaLegacy470 = true;
 
     environment.systemPackages = [
       #
