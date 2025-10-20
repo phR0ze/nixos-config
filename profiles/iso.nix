aa6d61754b7e7f558cd0b9368c59c22ae481428c
phR0ze <prc451@gmail.com>
2025-10-19 23:34:31 -0600

    Use offline clu for install

--- profiles/iso.nix
+++ profiles/iso.nix
@@ -51,36 +51,18 @@ in
     network using `wpa_cli`. See the NixOS manual for details.
   '';
 
-  # Add a marker to pass ISO state to install
-  environment.etc."iso-build-profile".text = args.target;
-
   # Adding packages for the ISO environment
   environment.systemPackages = with pkgs; [
     git         # dependency for clu installer automation
     jq          # dependency for clu installer automation
   ];
 
+  # Add a marker to pass ISO state to install
+  environment.etc."iso-build-profile".text = args.target;
+
   # Configure /etc/bashrc to launch our installer automation 'clu'
   programs.bash.promptPluginInit = ''
-    # Wait for the network to be live
-    echo ":: Checking if network is ready"
-    while ! ping -c 4 google.com &>/dev/null; do
-      echo "   Waiting on the network"
-      sleep 1
-    done
-    echo "   Network is ready"
-
-    # Clone the installer repo as needed
-    echo ":: Checking for the nixos-config repo"
-    if [ ! -d /home/nixos/nixos-config ]; then
-      echo "   Downloading https://github.com/phR0ze/nixos-config"
-      git clone https://github.com/phR0ze/nixos-config /home/nixos/nixos-config
-    fi
-    [ -f /home/nixos/nixos-config/clu ] && echo "   Installer script exists"
-
-    # Execute the installer script
     echo ":: Executing the installer script"
-    chmod +x /home/nixos/nixos-config/clu
-    sudo /home/nixos/nixos-config/clu install
+    sudo ${pkgs.clu}/bin/clu install
   '';
 }
