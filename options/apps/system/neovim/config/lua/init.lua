52207bf29f782fe4b307d86956ece56c74de212f
phR0ze <prc451@gmail.com>
2025-10-21 18:19:53 -0600

    Working on Neovim config

--- options/apps/system/neovim/config/lua/init.lua
+++ options/apps/system/neovim/config/lua/init.lua
@@ -1,7 +1,7 @@
 -- Initialize configuration
 require("config.options")               -- Load options from ../lua/config/options.lua
-require("config.keymaps")               -- load keymaps from ../lua/config/keymaps.lua
-require("config.autocmds")              -- load keymaps from ../lua/config/autocmds.lua
+--require("config.keymaps")               -- load keymaps from ../lua/config/keymaps.lua
+--require("config.autocmds")              -- load keymaps from ../lua/config/autocmds.lua
 
 -- Initialize plugin loading
 vim.cmd.packadd("lz.n")                 -- don't rely on nvim to load lz.n
---
+++ options/apps/system/neovim/config/lua/plugins/coding.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-autopairs",
+    event = "InsertEnter",
+    after = function()
+      require("nvim-autopairs").setup()
+    end,
+  },
+}
