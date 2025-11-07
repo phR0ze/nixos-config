8c6cf463604b3c13dc45e9893c486323d0d1c5d4
phR0ze <prc451@gmail.com>
2025-11-06 21:01:23 -0700

    Working TS and Oil

--- options/apps/system/neovim/config/lua/plugins/0000-nvim-treesitter.lua
+++ options/apps/system/neovim/config/lua/plugins/0000-nvim-treesitter.lua
@@ -1,14 +1,12 @@
 return {
-  -- Test by running :TSModuleInfo
   -- [Nvim-nix guide](https://github.com/adman44532/nvim-nix)
+  -- modern, faster more accurate syntax highlighting and code traversal
   {
     "nvim-treesitter",
     after = function()
       require("nvim-treesitter.configs").setup({
         auto_install = false,                         -- manage parser installs with Nix
         sync_install = false,                         -- manage parser installs with Nix
-        modules = {},
-        ignore_install = {},
         context_commentstring = {
           enable = true,
         },
@@ -20,9 +18,24 @@ return {
           enable = true,                              -- enable treesitter highlighting
           additional_vim_regex_highlighting = false,  -- disable default regex highlighting
         },
+
+        -- incremental selection allows you to select code blocks in an expanding or contracting way 
+        -- with hot keys to quickly select code blocks.
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<Enter>",
+            node_incremental = "<Enter>",
+            scope_incremental = false,
+            node_decremental = "<Backspace>",
+          },
+        },
       })
     end,
   },
+
+  -- TreeSitter context provides a context line at the top of your editor that shows the surrounding 
+  -- code object e.g. function definition if your in the function and scrolling off the window.
   {
     "nvim-treesitter-context",
     event = "DeferredUIEnter",
@@ -31,15 +44,17 @@ return {
     end,
     after = function()
       require("treesitter-context").setup({
-        enable = true,
-        multiwindow = false,
-        max_lines = 8, -- or 3 ?
-        min_window_height = 16,
-        line_numbers = true,
-        mode = "cursor",
+        enable = true,                                -- enable this plugin
+        multiwindow = false,                          -- disable multi-window support
+        max_lines = 3,                                -- how many lines the window should span
+        min_window_height = 16,                       -- minimum window height required to use context
+        line_numbers = true,                          -- rewrite the line numbers to reflect the correct lines
+        mode = "cursor",                              -- calculate context based on cusor location
       })
     end,
   },
+
+  -- TreeSitter TextObjects
   {
     "nvim-treesitter-textobjects",
     event = "DeferredUIEnter",
@@ -48,111 +63,45 @@ return {
     end,
     after = function()
       require("nvim-treesitter.configs").setup({
-        textobjects = {},
-      })
-    end,
-    binds = {},
-  },
-  -- {
-  --   "nvim-treesitter",
-  --   event = { "BufReadPost", "BufNewFile" },
-  --   after = function()
-  --     require("nvim-treesitter.configs").setup({
-  --       highlight = { enable = true },
-  --       indent = { enable = true },
-  --       auto_install = true,
-  --       ensure_installed = {
-  --         "bash", "c", "cpp", "css", "go", "html", "json", "javascript", "lua", 
-  --         "nix", "python", "rust", "toml", "typescript",
-  --       },
-  --     })
-  --   end,
-  -- },
-  -- {
-  --   "nvim-treesitter",
-  --   event = "FileType",
-  --   after = function()
-  --     require("nvim-treesitter.configs").setup({
-  --       highlight = { enable = true, additional_vim_regex_highlighting = false },
-  --       ensure_installed = { "c", "lua", "rust", "nix", "toml", "yaml", "json" },
-  --       auto_install = true,
-  --       incremental_selection = {
-  --         enable = true,
-  --         keymaps = {
-  --           init_selection = false,
-  --           node_decremental = "<A-CR>",
-  --           node_incremental = "<CR>",
-  --           -- scope_incremental = "grc",
-  --         },
-  --       },
-  --       indent = { enable = true },
-  --     })
-  --   end,
-  -- },
+        textobjects = {
 
-  -- {
-  --   "nvim-treesitter-textobjects",
-  --   event = { "BufReadPre", "BufNewFile" },
-  --   after = function()
-  --     ---@diagnostic disable-next-line: missing-fields
-  --     require("nvim-treesitter.configs").setup({
-  --       ensure_installed = { "c", "lua", "rust", "nix", "toml" },
-  --       auto_install = true,
-  --       highlight = {
-  --         enable = true,
-  --       },
-  --       indent = {
-  --         enable = true,
-  --       },
-  --       textobjects = {
-  --         select = {
-  --           enable = true,
-  --           lookahead = true,
-  --
-  --           keymaps = {
-  --             ["af"] = "@function.outer",
-  --             ["if"] = "@function.inner",
-  --             ["ac"] = "@class.outer",
-  --             ["ic"] = "@class.inner",
-  --             ["ia"] = "@parameter.inner",
-  --             ["aa"] = "@parameter.outer",
-  --             ["ix"] = "@comment.inner",
-  --             ["ax"] = "@comment.outer",
-  --           },
-  --         },
-  --         swap = {
-  --           enable = true,
-  --           swap_next = {
-  --             ["<leader>a"] = "@parameter.inner",
-  --           },
-  --           swap_previous = {
-  --             ["<leader>A"] = "@parameter.inner",
-  --           },
-  --         },
-  --         move = {
-  --           enable = true,
-  --           set_jumps = true,
-  --           goto_next_start = {
-  --             ["]a"] = "@parameter.inner",
-  --             ["]f"] = "@function.outer",
-  --           },
-  --           goto_next_end = {
-  --             ["]A"] = "@parameter.outer",
-  --             ["]F"] = "@function.outer",
-  --           },
-  --           goto_previous_start = {
-  --             ["[a"] = "@parameter.outer",
-  --             ["[f"] = "@function.outer",
-  --           },
-  --           goto_previous_end = {
-  --             ["[A"] = "@parameter.outer",
-  --             ["[F"] = "@function.outer",
-  --           },
-  --         },
-  --       },
-  --     })
-  --
-  --     vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = false })
-  --   end,
-  -- },
+          -- define text object selection keymaps and behavior
+          select = {
+            enable = true,
+            lookahead = true,
+
+            -- Select code. You must be in visual mode before these take affect
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["ia"] = "@parameter.inner",
+              ["aa"] = "@parameter.outer",
+              ["ix"] = "@comment.inner",
+              ["ax"] = "@comment.outer",
+            },
+          },
+
+          -- define code swap
+          swap = {
+            enable = true,
+            swap_next = { ["<leader>a"] = "@parameter.inner", },
+            swap_previous = { ["<leader>A"] = "@parameter.inner", },
+          },
+
+          -- define code navigation hot keys
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+            goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+            goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+            goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+          },
+        },
+      })
+      vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = false })
+    end,
+  },
 }
---
+++ options/apps/system/neovim/config/lua/plugins/0200-oil-nvim.lua
@@ -0,0 +1,38 @@
+vim.api.nvim_create_autocmd("User", {
+  pattern = "OilActionsPost",
+  callback = function(event)
+    if event.data.actions.type == "move" then
+      Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+    end
+  end,
+})
+
+return {
+  -- File manager for Neovim
+  "oil.nvim",
+  cmd = "Oil",
+  after = function()
+    require("oil").setup({
+      default_file_explorer = true,               -- replace the default file explorer
+      skip_confirm_for_simple_edits = true,       -- skip asking for confirmation
+      delete_to_trash = true,                     -- where is the trash?
+      columns = {
+        "permissions",                            -- display file permissions
+        "size",                                   -- display file size
+        "icon",                                   -- display file name
+      },
+      view_options = {
+        show_hidden = true,                       -- show dot prefixed files
+      },
+      float = {
+        padding = 8,                              -- vertical and horizontal padding for float window
+      },
+      keymaps = {
+        ["<ESC>"] = "actions.close",              -- ESC to close
+      },
+    })
+  end,
+  keys = {
+    { "-", "<cmd>Oil --float<cr>", desc = "Open oil" },   -- '-' to launch
+  },
+}
