a0552905c690701812547cf8216a4048a68597e2
phR0ze <prc451@gmail.com>
2025-11-09 21:46:32 -0700

    Working on writing mode

--- options/apps/system/neovim/writer.nix
+++ options/apps/system/neovim/writer.nix
@@ -1,13 +1,25 @@
-# Neovim configuration for writing
+# Neovim configuration
+#
+# ### References
+# - [Lazyvim Nix](https://github.com/jla2000/lazyvim-nix)
+# - [Nvim-bundle](https://github.com/jla2000/nvim-bundle)
+# - [Neovim Wrapper](https://ayats.org/blog/neovim-wrapper)
+# - [NixVim](https://github.com/nix-community/nixvim)
+# - [Frosty Vim](https://github.com/SystematicError/frosty-vim)
+# - [Lz.n](https://github.com/lumen-oss/lz.n)
 #
 # ### Details
-# - see default.nix for more details on how this was built
+# - Configuration is all written in lua
+# - Plugins are lazy loaded using lz.n
 #---------------------------------------------------------------------------------------------------
 {
   neovim-unwrapped, symlinkJoin, fetchFromGitHub, makeWrapper, runCommandLocal, vimPlugins, vimUtils,
-  writeTextFile, lib,
+  writeTextFile, lib, pkgs
 }: let
 
+  # Plugins to make available but don't load on boot.
+  # - these will be lazy loaded by lz-n as needed based on triggers
+  # - end up in ./result/pack/plugins/opt
   plugins = with vimPlugins; [
     lz-n                            # Light weight lazy plugin loader
 
@@ -17,10 +29,15 @@
     tokyonight-nvim                 # A clean dark Neovim theme
     catppuccin-nvim                 # Soothing pastel theme for Neovim
 
+    # Modern, minimal, pure lua replacement for nvim-web-devicons
+    # - configuration ./config/lua/plugins/0000-mini-icons.lua
+    # - can patch itself into plugins expecting nvim-web-devicons thus eliminating the need for both
+    # - no external dependencies
     (vimUtils.buildVimPlugin {
       pname = "mini.icons"; version = "2025-10-30";
       src = fetchFromGitHub { owner = "nvim-mini"; repo = "mini.icons";
-        rev = "v0.16.0"; sha256 = "sha256-/sdLtMOOGeVvFDBB9N4CyUHpGXtUi1ZJ9dIpvxZ9C4Q="; };})
+        rev = "v0.16.0"; sha256 = "sha256-/sdLtMOOGeVvFDBB9N4CyUHpGXtUi1ZJ9dIpvxZ9C4Q="; };
+      doCheck = false; doNvimRequireCheck = false; })
 
     # Intelligent pairing, better than mini.pairs
     # - configuration ./config/lua/plugins/0000-nvim-autopairs.lua
@@ -30,36 +47,65 @@
       pname = "nvim-autopairs"; version = "2025-10-30";
       src = fetchFromGitHub { owner = "windwp"; repo = "nvim-autopairs";
         rev = "7a2c97cccd60abc559344042fefb1d5a85b3e33b";
-        sha256 = "sha256-cRIg1qO3WMxzcDQti0GEJl77KnlRCqyBN+g76PviWt0="; };})
+        sha256 = "sha256-cRIg1qO3WMxzcDQti0GEJl77KnlRCqyBN+g76PviWt0="; };
+      doCheck = false; doNvimRequireCheck = false; })
 
     # Persistence is a simple lua plugin for automated session management.
     # - configuration ./config/lua/plugins/0000-persistence-nvim.lua
     # - saves active session under ~/.local/state/nvim/sessions on exit
+    # - no external dependencies
     (vimUtils.buildVimPlugin {
       pname = "persistence.nvim"; version = "2025-10-30";
       src = fetchFromGitHub { owner = "folke"; repo = "persistence.nvim";
         rev = "v3.1.0"; sha256 = "sha256-xZij+CYjAoxWcN/Z2JvJWoNkgBkz83pSjUGOfc9x8M0="; };})
 
-    # # Installs and manages languages parsers and provides this syntax tree to other plugins
-    # (vimUtils.buildVimPlugin {
-    #   pname = "nvim-treesitter"; version = "2025-10-30";
-    #   src = fetchFromGitHub { owner = "nvim-treesitter"; repo = "nvim-treesitter";
-    #     rev = "42fc28ba918343ebfd5565147a42a26580579482";
-    #     sha256 = "sha256-CVs9FTdg3oKtRjz2YqwkMr0W5qYLGfVyxyhE3qnGYbI="; };})
-    #
-    # # Builds on treesitter to provide context about your code objects
-    # (vimUtils.buildVimPlugin {
-    #   pname = "nvim-treesitter-context"; version = "2025-10-30";
-    #   src = fetchFromGitHub { owner = "nvim-treesitter"; repo = "nvim-treesitter-context";
-    #     rev = "ec308c7827b5f8cb2dd0ad303a059c945dd21969";
-    #     sha256 = "sha256-QdZstxKsEILwe7eUZCmMdyLPyvNKc/e7cfdYQowHWPQ="; };})
-    #
-    # # Builds on treesitter to provide code navigation motions 
-    # (vimUtils.buildVimPlugin {
-    #   pname = "nvim-treesitter-textobjects"; version = "2025-10-30";
-    #   src = fetchFromGitHub { owner = "nvim-treesitter"; repo = "nvim-treesitter-textobjects";
-    #     rev = "5ca4aaa6efdcc59be46b95a3e876300cfead05ef";
-    #     sha256 = "sha256-lf+AwSu96iKO1vWWU2D7jWHGfjXkbX9R2CX3gMZaD4M="; };})
+    # Include nvim-treesitter (TS) parsers along with the plugin
+    # - modern parser for faster, more accurate syntax highlighting and language awareness
+    # - parsers get included at `nvim-treesitter/parser/` which is autoloaded by TS
+    # - all grammars can be included with nvim-treesitter.withAllGrammars.dependencies
+    # - no external dependencies
+    (symlinkJoin {
+      name = "nvim-treesitter"; # use the same name to match plugin name expectations
+      paths = [ nvim-treesitter (nvim-treesitter.withPlugins (x: [
+        x.bash
+        x.diff
+        x.dart
+        x.html
+        x.javascript
+        x.jsdoc
+        x.json
+        x.lua
+        x.luadoc
+        x.markdown
+        x.markdown_inline
+        x.printf
+        x.python
+        x.regex
+        x.rust
+        x.toml
+        x.vim
+        x.vimdoc
+        x.xml
+        x.yaml
+      ])).dependencies];
+    })
+
+    # Builds on treesitter to provide context about your code objects
+    # - depends on treesitter
+    (vimUtils.buildVimPlugin {
+      pname = "nvim-treesitter-context"; version = "2025-10-30";
+      src = fetchFromGitHub { owner = "nvim-treesitter"; repo = "nvim-treesitter-context";
+        rev = "ec308c7827b5f8cb2dd0ad303a059c945dd21969";
+        sha256 = "sha256-QdZstxKsEILwe7eUZCmMdyLPyvNKc/e7cfdYQowHWPQ="; };})
+
+    # Builds on treesitter to provide code navigation motions 
+    # - depends on treesitter
+    (vimUtils.buildVimPlugin {
+      pname = "nvim-treesitter-textobjects"; version = "2025-10-30";
+      src = fetchFromGitHub { owner = "nvim-treesitter"; repo = "nvim-treesitter-textobjects";
+        rev = "5ca4aaa6efdcc59be46b95a3e876300cfead05ef";
+        sha256 = "sha256-lf+AwSu96iKO1vWWU2D7jWHGfjXkbX9R2CX3gMZaD4M="; };
+      doCheck = false; doNvimRequireCheck = false; })
 
     # Better comments and override support for treesitter languages
     # - configuration ./config/lua/plugins/0000-ts-comments-nvim.lua
@@ -76,7 +122,8 @@
     (vimUtils.buildVimPlugin {
       pname = "bufferline.nvim"; version = "2025-10-30";
       src = fetchFromGitHub { owner = "akinsho"; repo = "bufferline.nvim";
-        rev = "v4.9.1"; sha256 = "sha256-ae4MB6+6v3awvfSUWlau9ASJ147ZpwuX1fvJdfMwo1Q="; };})
+        rev = "v4.9.1"; sha256 = "sha256-ae4MB6+6v3awvfSUWlau9ASJ147ZpwuX1fvJdfMwo1Q="; };
+      doCheck = false; doNvimRequireCheck = false; })
 
     # Blazing fast neovim statusline written in pure lua
     # - configuration ./config/lua/plugins/0100-lualine-nvim.lua
@@ -93,7 +140,8 @@
     (vimUtils.buildVimPlugin {
       pname = "snacks.nvim"; version = "2025-10-30";
       src = fetchFromGitHub { owner = "folke"; repo = "snacks.nvim";
-        rev = "v2.27.0"; sha256 = "sha256-QkcOKPgiJeA5LmQvC7DZ6ddjaoW8AE5I08Gm8jlEkT8="; };})
+        rev = "v2.27.0"; sha256 = "sha256-QkcOKPgiJeA5LmQvC7DZ6ddjaoW8AE5I08Gm8jlEkT8="; };
+      doCheck = false; doNvimRequireCheck = false; })
 
     # Pops up a window on different hot keys that shows keymaps as you are typing
     # - configuration ./config/lua/plugins/0100-which-key.lua
@@ -101,53 +149,77 @@
     (vimUtils.buildVimPlugin {
       pname = "which-key.nvim"; version = "2025-10-30";
       src = fetchFromGitHub { owner = "folke"; repo = "which-key.nvim";
-        rev = "v3.17.0"; sha256 = "sha256-kYpiw2Syu54B/nNVqTZeUHJIPNzAv3JpFaMWR9Ai3p4="; };})
+        rev = "v3.17.0"; sha256 = "sha256-kYpiw2Syu54B/nNVqTZeUHJIPNzAv3JpFaMWR9Ai3p4="; };
+      doCheck = false; doNvimRequireCheck = false; })
 
-
-
-
-
-
-
+    # Navigate code with search labels, enhanced character motions and Treesitter integration 
+    # - configuration ./config/lua/plugins/0100-flash-nvim.lua
+    # - depends on treesitter
     (vimUtils.buildVimPlugin {
       pname = "flash.nvim"; version = "2025-10-30";
       src = fetchFromGitHub { owner = "folke"; repo = "flash.nvim";
-        rev = "v2.1.0"; sha256 = "sha256-Qh9ty28xtRS3qXxE/ugx9FqAKrdeFGEf7W6yEORnZV8="; };})
+        rev = "v2.1.0"; sha256 = "sha256-Qh9ty28xtRS3qXxE/ugx9FqAKrdeFGEf7W6yEORnZV8="; };
+        doCheck = false; doNvimRequireCheck = false; })
 
+    # Fast fuzzy finding - better than telescope
+    # - configuration ./config/lua/plugins/0100-fzf-lua.lua
+    # - depends on mini.icons
     (vimUtils.buildVimPlugin {
-      pname = "todo-comments.nvim"; version = "2025-10-30";
-      src = fetchFromGitHub { owner = "folke"; repo = "todo-comments.nvim";
-        rev = "v1.4.0"; sha256 = "sha256-EH4Sy7qNkzOgA1INFzrtsRfD79TgMqSbKUdundyw22w="; };})
+      pname = "fzf-lua"; version = "2025-10-30";
+      src = fetchFromGitHub { owner = "ibhagwan"; repo = "fzf-lua";
+        rev = "a8458b79a957a6e3e217d84106a0fd4b9470ff4c";
+        sha256 = "sha256-OloQKfJZIP8Hu9O6+b2iVUtaD52dyYrfDy16Kd3KXHM="; };
+        doCheck = false; doNvimRequireCheck = false; })
 
+    # Twilight is a Lua plugin for Neovim 0.5 that dims inactive portions of the code you're editing.
+    # - configuration ./config/lua/plugins/0100-twilight-nvim.lua
+    # - depends on treesitter
+    (vimUtils.buildVimPlugin {
+      pname = "twilight.nvim"; version = "2025-10-30";
+      src = fetchFromGitHub { owner = "folke"; repo = "twilight.nvim";
+        rev = "664e752f4a219801265cc3fc18782b457b58c1e1";
+        sha256 = "sha256-V6DFwvShvX6mYMRJajwOaxbHMNuCHCZzVrfT73iMuQo="; };})
 
-           # Utility
+    # File explorer that lets you edit your file system like a buffer
+    # - configuration ./config/lua/plugins/0200-oil-nvim.lua
+    # - depends on mini.icons and snacks
+    (vimUtils.buildVimPlugin {
+      pname = "oil.nvim"; version = "2025-10-30";
+      src = fetchFromGitHub { owner = "stevearc"; repo = "oil.nvim";
+        rev = "v2.15.0"; sha256 = "sha256-jCuOwTd6QIy3USuE7X/rHYUbriW6+2WaK9JxCx4/O2c="; };
+      doCheck = false; doNvimRequireCheck = false; })
+
+    # Zen mode for distraction free writing
+    # - configuration ./config/lua/plugins/0200-zen-mode-nvim.lua
+    # - Open current buffer in new full-screen floating window
+    (vimUtils.buildVimPlugin {
+      pname = "zen-mode.nvim"; version = "2025-10-30";
+      src = fetchFromGitHub { owner = "folke"; repo = "zen-mode.nvim";
+        rev = "v1.4.1"; sha256 = "sha256-vRJynz3bnkhfHKya+iEgm4PIEwT2P9kvkskgTt5UUU4="; };})
 
     #codecompanion-nvim
     #plenary-nvim
-    #telescope-nvim
     #vim-startuptime
   ];
 
-  # Build the plugins package for Neovim
-  # - Packages are a named collection of plugins organized by purpose i.e. MODE=('start' | 'opt')
+  # Name to give to this custom version of Neovim
+  appName = "nvim-custom";
+
+  # Build the plugins into a package for Neovim
+  # - Packages are a named collection of plugins organized by mode i.e. MODE=('start' | 'opt')
   # - Packages are in the `packpath` under `pack/$NAME/$MODE/{$PLUGIN_1...$PLUGIN_n}`
   # - Plugins under the `start` sub folder are automatically loaded by nvim on boot
   # - Plugins under `opt` are available to be loaded manually with `:packadd` from within nvim
   # - All .lua files in any pack/$NAME/start/$PLUGIN/plugin/ path will be automatically loaded
   # - Lua modules are looked for in any `lua` sub-folders in any path in the `runtimepath`
   # - Lua require calls reference the folder structure e.g. `require('a.b')` for `lua/a/b.lua`
-  packName = "plugins";                     # can be anything
-  pluginPath = runCommandLocal "nvim-plugins" {} ''
-    mkdir -p $out/pack/${packName}/{start,opt}
+  pluginPkg = runCommandLocal "nvim-plugins" {} ''
+    mkdir -p $out/pack/${appName}/{start,opt}
     ln -vsfT ${./config/lua} $out/lua
-    ln -vsfT ${./config/init} $out/pack/${packName}/start/init
+    ln -vsfT ${./config/init} $out/pack/${appName}/start/init
     ${
       lib.concatMapStringsSep "\n"
-
-      # Include the plugin with the appropriate folder structure
-      (plugin: "ln -vsfT ${plugin} $out/pack/${packName}/opt/${lib.getName plugin}")
-
-      # Plugins list to run through
+      (plugin: "ln -vsfT ${plugin} $out/pack/${appName}/opt/${lib.getName plugin}")
       plugins
     }
   '';
@@ -155,8 +227,7 @@
   # Create a simple lua init script
   # Not using this in favor of my custom plugin ./config/init which loads ./config/lua/init.lua to
   # configure Nvim with all options, keymaps, plugins etc... instead of inline like it is here. The 
-  # benefits are you get Lua syntax highlighting and full Lua module support which this inline here 
-  # lacks.
+  # benefits are you get Lua syntax highlighting and full Lua module support which is lacking here.
   initLua = writeTextFile {
     name = "init.lua";
     text = ''
@@ -164,13 +235,23 @@
     '';
   };
 
+  # Runtime dependencies to support Neovim and Neovimthe  plugins
+  runtimeDeps = with pkgs; [
+    fzf                                     # Fast fuzzy finder: dep of fzf-lua
+    ripgrep                                 # Faster more capable grep: dep of fzf-lua
+    stylua                                  # Opinionated Lua code formatter
+  ];
+
 in 
   # symlinkJoin creates a new derivation that replaces all files in the given path with links where 
   # the files were moved to the store individually. This makes it easier to modify the original 
   # package while reusing its binaries to be efficient.
   symlinkJoin {
-    name = "neovim-custom";                 # Package name being created
-    paths = [neovim-unwrapped pluginPath];  # Paths being symlinked
+    name = appName;                         # Package name being created
+    paths = [                               # Paths being symlinked
+      neovim-unwrapped                      # Neovim and its files
+      pluginPkg                             # Plugin files
+    ] ++ runtimeDeps;                       # Include other runtime dependencies
     nativeBuildInputs = [makeWrapper];      # Build tools to make available during build
 
     # Wrap the target passing in custom arguments
@@ -181,17 +262,18 @@ in
     # - Also create the alias links
     postBuild = ''
       wrapProgram $out/bin/nvim \
+        --prefix PATH : ${ lib.makeBinPath runtimeDeps } \
         --add-flags '-u' \
         --add-flags '${initLua}' \
         --add-flags '--cmd' \
-        --add-flags "'set packpath^=${pluginPath} | set runtimepath^=${pluginPath}'" \
-        --set-default NVIM_APPNAME nvim-custom
+        --add-flags "'set packpath^=${pluginPkg} | set runtimepath^=${pluginPkg}'" \
+        --set-default NVIM_APPNAME ${appName}
       ln -s $out/bin/nvim $out/bin/vim
     '';
 
     # Make the plugins derivation available for build commands like
     # nix build -f ./direct.nix plugins
     passthru = {
-      inherit pluginPath;
+      inherit pluginPkg;
     };
   }
