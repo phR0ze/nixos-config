3f61d22e78ef690114a5076de52963275ce3b50b
phR0ze <prc451@gmail.com>
2026-01-07 16:41:39 -0700

    Adding custom indenting for vim file types

--- options/apps/system/neovim/config/lua/config/autocmds.lua
+++ options/apps/system/neovim/config/lua/config/autocmds.lua
@@ -23,19 +23,72 @@ vim.api.nvim_create_autocmd("CmdlineChanged", {
   end,
 })
 
--- Set custom settings for markdown files
+----------------------------------------------------------------------------------------------------
+-- File type settings
+----------------------------------------------------------------------------------------------------
+
+-- html files
+local html_group = vim.api.nvim_create_augroup("HtmlSettings", { clear = true })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead", "FileType" }, {
+  pattern = { "html", "*.html" },
+  callback = function()
+    vim.bo.filetype = "html"
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+  group = html_group,
+})
+-- make files
+local make_group = vim.api.nvim_create_augroup("MakeSettings", { clear = true })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead", "FileType" }, {
+  pattern = { "make", "Makefile", "*.make", "*.makefile" },
+  callback = function()
+    vim.bo.filetype = "make"
+    vim.opt_local.expandtab = false -- don't expand tabs for Makefiles
+  end,
+  group = make_group,
+}
+
+-- markdown files
 local md_group = vim.api.nvim_create_augroup("MarkdownSettings", { clear = true })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead", "FileType" }, {
-  pattern = { "*.markdown", "*.mdown", "*.mkd", "*.mkdn", "*.mdwn", "*.md", "markdown" },
+  pattern = { "markdown", "*.md" },
   callback = function()
-    -- Ensure buffer filetype is markdown
     vim.bo.filetype = "markdown"
-
-    -- 2-space indentation
     vim.opt_local.tabstop = 2
     vim.opt_local.shiftwidth = 2
     vim.opt_local.softtabstop = 2
     vim.opt_local.expandtab = true
   end,
   group = md_group,
+})
+
+-- yaml files
+local yaml_group = vim.api.nvim_create_augroup("YamlSettings", { clear = true })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead", "FileType" }, {
+  pattern = { "yaml", "*.yaml" },
+  callback = function()
+    vim.bo.filetype = "yaml"
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+  group = yaml_group,
+})
+
+-- xml files
+local xml_group = vim.api.nvim_create_augroup("XmlSettings", { clear = true })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead", "FileType" }, {
+  pattern = { "xml", "*.xml", "*.menu" },
+  callback = function()
+    vim.bo.filetype = "xml"
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+  group = xml_group,
 })
--- options/apps/system/neovim/config/lua/init.lua
+++ options/apps/system/neovim/config/lua/init.lua
@@ -1,11 +1,11 @@
 -- Initialize configuration
-require("config.options")               -- Load options from ./config/options.lua
-require("config.keymaps")               -- load keymaps from ./config/keymaps.lua
---require("config.autocmds")              -- load keymaps from ./config/autocmds.lua
+require("config.options") -- Load options from ./config/options.lua
+require("config.keymaps") -- load keymaps from ./config/keymaps.lua
+require("config.autocmds") -- load keymaps from ./config/autocmds.lua
 
 -- Initialize plugin loading
-vim.cmd.packadd("lz.n")                 -- load our package manager
-require("lz.n").load("plugins")         -- load all other plugins from ./plugins
+vim.cmd.packadd("lz.n") -- load our package manager
+require("lz.n").load("plugins") -- load all other plugins from ./plugins
 
 -- Set the target color scheme
 vim.cmd.colorscheme("tokyonight-storm")
