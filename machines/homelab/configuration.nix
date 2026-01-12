bf7a1a8114cf43e99993ad0807706df56c2034e3
phR0ze <prc451@gmail.com>
2026-01-11 19:01:14 -0700

    Adding homelab to tailnet

--- machines/homelab/configuration.nix
+++ machines/homelab/configuration.nix
@@ -29,12 +29,14 @@
     services.raw.nix-cache.host.enable = true;
     services.raw.adguardhome.enable = true;
     services.raw.mullvad.enable = true;
+    services.raw.tailscale.enable = true;
     services.raw.synology-drive-client.enable = true;
     services.oci.homarr = { enable = true; port = 80; };
     services.oci.oneup = { enable = true; port = 8002; };
     services.oci.stirling-pdf = { enable = true; port = 8001; };
     services.oci.immich = { enable = true; port = 2283; tag = "v2.0.1"; };
 
+    # Additional apps
     environment.systemPackages = [
       pkgs.brave
     ];
