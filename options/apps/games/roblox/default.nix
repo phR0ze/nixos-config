4caa12aafbda85f3e162fe6ac951c323ce100d73
phR0ze <prc451@gmail.com>
2026-02-26 17:57:19 -0700

    Sober already has a desktop entry

--- options/apps/games/roblox/default.nix
+++ options/apps/games/roblox/default.nix
@@ -12,36 +12,11 @@ in
   };
 
   config = lib.mkMerge [
-
-    # Install roblox
     (lib.mkIf cfg.enable {
       apps.system.flatpak = {
         enable = true;
         packages = [{ appId = "org.vinegarhq.Sober"; }];
       };
-    })
-
-    # XFCE supporting configuration
-    (lib.mkIf (cfg.enable && xfce.enable) {
-      system.xdg.menu.itemOverrides = [
-        {
-          source = pkgs.writeTextFile {
-            name = "org.vinegarhq.Sober.desktop";
-            text = ''
-              [Desktop Entry]
-              Name=Roblox
-              Exec=flatpak run org.vinegarhq.Sober
-              Icon=org.vinegarhq.Sober
-              Terminal=false
-              Type=Application
-              Categories=Games;
-              StartupNotify=true
-              Comment=Play Roblox via Sober
-            '';
-          };
-          categories = "Games";
-        }
-      ];
     })
   ];
 }
