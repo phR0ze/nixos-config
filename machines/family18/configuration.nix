9b8f2581a7442e9922b6528ca7d44c5a2f076c47
phR0ze <28798432+phR0ze@users.noreply.github.com>
2026-03-28 17:47:14 -0600

    Adding family18 system

---
+++ machines/family18/configuration.nix
@@ -0,0 +1,23 @@
+# Family18 configuration
+#
+# ### Machine specs
+# - ?
+#
+# ### Features
+# - Basic desktop deployment
+# --------------------------------------------------------------------------------------------------
+{ ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../profiles/xfce/desktop.nix
+  ];
+
+  config = {
+    machine.type.bootable = true;
+    machine.nix.cache.enable = true;
+    devices.gpu.nvidia = { enable = true; open = true; };
+
+    apps.games.roblox.enable = true;
+  };
+}
