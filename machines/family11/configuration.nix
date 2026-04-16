8648b4a9594a7e6ea88e8fcfb55c74433a8d3613
phR0ze <prc451@gmail.com>
2026-04-15 21:27:10 -0600

    Working on Wifi stalling

--- machines/family11/configuration.nix
+++ machines/family11/configuration.nix
@@ -27,6 +27,15 @@
       ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="b812", TEST=="power/control", ATTR{power/control}="on"
     '';
 
+    # Disable rtw88 deep link power save (LPS-deep). This mode allows the radio
+    # to enter an aggressive low-power state during brief idle periods; under
+    # bursty traffic it stalls for hundreds of milliseconds waiting to wake back
+    # up. NetworkManager power save and USB autosuspend are already disabled
+    # above; this is the third layer needed for the RTL8822BU specifically.
+    boot.extraModprobeConfig = ''
+      options rtw88_core disable_lps_deep=Y
+    '';
+
     apps.dev.claude.enable = true;
 
     # Pre-generate thumbnails via tumblerd; run: gen-thumbs /mnt/Data
