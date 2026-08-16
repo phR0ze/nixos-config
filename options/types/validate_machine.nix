214853197ec9ef656b8afa2e87188985bd660f51
phR0ze <28798432+phR0ze@users.noreply.github.com>
2026-08-15 19:32:44 -0600

    Dropping old secrets array pattern

--- options/types/validate_machine.nix
+++ options/types/validate_machine.nix
@@ -76,7 +76,6 @@ in
     { assertion = (cfg.nix.cache.ip != null); message = "assert machine.nix.cache.ip: ${cfg.nix.cache.ip}"; }
 
     { assertion = (cfg.drives != null); message = "assert machine.drives: ${toString (builtins.length cfg.drives)}"; }
-    { assertion = (cfg.secrets != null); message = "assert machine.secrets: ${toString (builtins.length cfg.secrets)}"; }
     { assertion = (cfg.smb.entries != null); message = "assert machine.smb.entries: ${toString (builtins.length cfg.smb.entries)}"; }
     { assertion = (cfg.nfs.entries != null); message = "assert machine.nfs.entries: ${toString (builtins.length cfg.nfs.entries)}"; }
   ];
