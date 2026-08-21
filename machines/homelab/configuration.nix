dfb34fdb74397cb47ef0a5b415f7236942b594ed
phR0ze <prc451@gmail.com>
2026-08-20 18:02:18 -0600

    Only have vaultwarden listen on localhost

--- machines/homelab/configuration.nix
+++ machines/homelab/configuration.nix
@@ -31,9 +31,7 @@
     services.raw.mullvad.enable = true;
     services.raw.synology-drive-client.enable = true;
     services.raw.vaultwarden = {
-      enable = true;
-      port = 8222;
-      subdomain = "vault";
+      enable = true; port = 8222; subdomain = "vault";
     };
     services.oci.homarr = {
       enable = true; port = 8080; user.uid = 2000; subdomain = "home"; tag = "v1.37.0";
